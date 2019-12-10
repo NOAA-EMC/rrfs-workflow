f736ad7efd7f0eeffe30faa985f98ecac6f8db6a
gerard ketefian <gerard.ketefian@noaa.gov>
2019-12-10 15:42:31 -0700

    Use the get_manage_externals_config_property function to extract from the Externals.cfg configuration file the directory in which the forecast model's repository is cloned.  This replaces a hard-coded directory name.

--- sorc/build_forecast.sh
+++ sorc/build_forecast.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 set -eux
 
+. ../ush/source_util_funcs.sh
+
 source ./machine-setup.sh > /dev/null 2>&1
 cwd=`pwd`
 
@@ -18,7 +20,29 @@ fi
 
 if [ $target = hera ]; then target=hera.intel ; fi
 
-cd NEMSfv3gfs/
+#------------------------------------
+# Get from the manage_externals configuration file the relative directo-
+# ries in which the UFS utility codes (not including chgres_cube) and 
+# the chgres_cube codes get cloned.  Note that these two sets of codes
+# are in the same repository but different branches.  These directories
+# will be relative to the workflow home directory, which we denote below
+# by HOMErrfs.  Then form the absolute paths to these codes.
+#------------------------------------
+HOMErrfs=$( readlink -f "${cwd}/.." )
+mng_extrns_cfg_fn="${HOMErrfs}/Externals.cfg"
+property_name="local_path"
+
+# First, consider the UFS utility codes, not including chgres (i.e. we
+# do not use any versions of chgres or chgres_cube in this set of codes).
+external_name="ufs_weather_model"
+forecast_model_dir=$( \
+get_manage_externals_config_property \
+"${mng_extrns_cfg_fn}" "${external_name}" "${property_name}" ) || \
+print_err_msg_exit "\
+Call to function get_manage_config_externals_property failed."
+forecast_model_dir="${HOMErrfs}/${forecast_model_dir}"
+
+cd ${forecast_model_dir}
 FV3=$( pwd -P )/FV3
 CCPP=${CCPP:-"false"}
 cd tests/
