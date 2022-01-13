0e476f312d88016aa4543bde54421b8e84a461d0
Ming Hu <Ming.Hu@noaa.gov>
2022-01-13 15:53:37 -0700

    Need to consider EXTRN_MDL_ICS_OFFSET_HRS when calculates cycle hour for make_ics (#264)

--- scripts/exregional_make_ics.sh
+++ scripts/exregional_make_ics.sh
@@ -542,9 +542,21 @@ esac
 #
 #-----------------------------------------------------------------------
 #
+yyyymmdd="${EXTRN_MDL_CDATE:0:8}"
 mm="${EXTRN_MDL_CDATE:4:2}"
 dd="${EXTRN_MDL_CDATE:6:2}"
 hh="${EXTRN_MDL_CDATE:8:2}"
+
+fhr="${EXTRN_MDL_ICS_OFFSET_HRS}"
+cdate_crnt_fhr=$( date --utc --date "${yyyymmdd} ${hh} UTC + ${fhr} hours" "+%Y%m%d%H" )
+#
+# Get the month, day, and hour corresponding to the current forecast time
+# of the the external model.
+#
+  mm="${cdate_crnt_fhr:4:2}"
+  dd="${cdate_crnt_fhr:6:2}"
+  hh="${cdate_crnt_fhr:8:2}"
+
 #
 #-----------------------------------------------------------------------
 #
