760a3db4108f53c17336e3bfe50f932edf36e8fd
Ming Hu <Ming.Hu@noaa.gov>
2022-05-31 00:19:30 -0600

    Save ncdiag files to satbias directory. (#392)

--- scripts/exregional_run_analysis.sh
+++ scripts/exregional_run_analysis.sh
@@ -702,17 +702,17 @@ if [ ${DO_RADDA} == "TRUE" ]; then
 
   ## if satbias files (go back to previous 10 dyas) are not available from ${satbias_dir}, use satbias files from the ${FIX_GSI} 
   if [ $satcounter -eq $maxcounter ]; then
-    if [ -r ${FIX_GSI}/rrfs.starting_satbias ]; then
-      echo "using satllite satbias_in files from ${FIX_GSI}"     
-      cp_vrfy ${FIX_GSI}/rrfs.starting_satbias ./satbias_in
+    if [ -r ${FIX_GSI}/rrfs.gdas_satbias ]; then
+      echo "using satllite gdas satbias_in files from ${FIX_GSI}"     
+      cp_vrfy ${FIX_GSI}/rrfs.gdas_satbias ./satbias_in
     fi
-    if [ -r ${FIX_GSI}/rrfs.starting_satbias_pc ]; then
-      echo "using satllite satbias_pc files from ${FIX_GSI}"     
-      cp_vrfy ${FIX_GSI}/rrfs.starting_satbias_pc ./satbias_pc
+    if [ -r ${FIX_GSI}/rrfs.gdas_satbias_pc ]; then
+      echo "using satllite gdas satbias_pc files from ${FIX_GSI}"     
+      cp_vrfy ${FIX_GSI}/rrfs.gdas_satbias_pc ./satbias_pc
     fi
-    if [ -r ${FIX_GSI}/rrfs.starting_radstat ]; then
-      echo "using satllite radstat files from ${FIX_GSI}"     
-      cp_vrfy ${FIX_GSI}/rrfs.starting_radstat ./radstat.rrfs
+    if [ -r ${FIX_GSI}/rrfs.gdas_radstat ]; then
+      echo "using netcdf satllite radstat files from ${FIX_GSI}"     
+      cp_vrfy ${FIX_GSI}/rrfs.gdas_radstat ./radstat.rrfs
     fi
   fi
 
@@ -723,7 +723,7 @@ if [ ${DO_RADDA} == "TRUE" ]; then
     date=`echo $diag_file | cut -d'.' -f2`
     gunzip $diag_file
     fnameanl=$(echo $fname|sed 's/_ges//g')
-    mv $fname.$date $fnameanl
+    mv $fname.$date* $fnameanl
   done
 fi
 
@@ -849,18 +849,24 @@ case $loop in
 esac
 
 #  Collect diagnostic files for obs types (groups) below
+numfile_rad_bin=0
+numfile_cnv=0
+numfile_rad=0
 if [ $binary_diag = ".true." ]; then
    listall="hirs2_n14 msu_n14 sndr_g08 sndr_g11 sndr_g11 sndr_g12 sndr_g13 sndr_g08_prep sndr_g11_prep sndr_g12_prep sndr_g13_prep sndrd1_g11 sndrd2_g11 sndrd3_g11 sndrd4_g11 sndrd1_g15 sndrd2_g15 sndrd3_g15 sndrd4_g15 sndrd1_g13 sndrd2_g13 sndrd3_g13 sndrd4_g13 hirs3_n15 hirs3_n16 hirs3_n17 amsua_n15 amsua_n16 amsua_n17 amsua_n18 amsua_n19 amsua_metop-a amsua_metop-b amsua_metop-c amsub_n15 amsub_n16 amsub_n17 hsb_aqua airs_aqua amsua_aqua imgr_g08 imgr_g11 imgr_g12 pcp_ssmi_dmsp pcp_tmi_trmm conv sbuv2_n16 sbuv2_n17 sbuv2_n18 omi_aura ssmi_f13 ssmi_f14 ssmi_f15 hirs4_n18 hirs4_metop-a mhs_n18 mhs_n19 mhs_metop-a mhs_metop-b mhs_metop-c amsre_low_aqua amsre_mid_aqua amsre_hig_aqua ssmis_las_f16 ssmis_uas_f16 ssmis_img_f16 ssmis_env_f16 iasi_metop-a iasi_metop-b iasi_metop-c seviri_m08 seviri_m09 seviri_m10 seviri_m11 cris_npp atms_npp ssmis_f17 cris-fsr_npp cris-fsr_n20 atms_n20 abi_g16 abi_g17"
    for type in $listall; do
       count=$(ls pe*.${type}_${loop} | wc -l)
       if [[ $count -gt 0 ]]; then
          $(cat pe*.${type}_${loop} > diag_${type}_${string}.${YYYYMMDDHH})
+         echo "diag_${type}_${string}.${YYYYMMDDHH}" >> listrad_bin
+         numfile_rad_bin=`expr ${numfile_rad_bin} + 1`
       fi
    done
 fi
 
 if [ $netcdf_diag = ".true." ]; then
-   listallnc="conv_ps conv_q conv_t conv_uv"
+   listall_cnv="conv_ps conv_q conv_t conv_uv conv_pw conv_rw conv_sst"
+   listall_rad="hirs2_n14 msu_n14 sndr_g08 sndr_g11 sndr_g11 sndr_g12 sndr_g13 sndr_g08_prep sndr_g11_prep sndr_g12_prep sndr_g13_prep sndrd1_g11 sndrd2_g11 sndrd3_g11 sndrd4_g11 sndrd1_g15 sndrd2_g15 sndrd3_g15 sndrd4_g15 sndrd1_g13 sndrd2_g13 sndrd3_g13 sndrd4_g13 hirs3_n15 hirs3_n16 hirs3_n17 amsua_n15 amsua_n16 amsua_n17 amsua_n18 amsua_n19 amsua_metop-a amsua_metop-b amsua_metop-c amsub_n15 amsub_n16 amsub_n17 hsb_aqua airs_aqua amsua_aqua imgr_g08 imgr_g11 imgr_g12 pcp_ssmi_dmsp pcp_tmi_trmm conv sbuv2_n16 sbuv2_n17 sbuv2_n18 omi_aura ssmi_f13 ssmi_f14 ssmi_f15 hirs4_n18 hirs4_metop-a mhs_n18 mhs_n19 mhs_metop-a mhs_metop-b mhs_metop-c amsre_low_aqua amsre_mid_aqua amsre_hig_aqua ssmis_las_f16 ssmis_uas_f16 ssmis_img_f16 ssmis_env_f16 iasi_metop-a iasi_metop-b iasi_metop-c seviri_m08 seviri_m09 seviri_m10 seviri_m11 cris_npp atms_npp ssmis_f17 cris-fsr_npp cris-fsr_n20 atms_n20 abi_g16"
 
    cat_exec="${EXECDIR}/nc_diag_cat.x"
 
@@ -875,10 +881,25 @@ if [ $netcdf_diag = ".true." ]; then
         Build GSI and rerun."
    fi
 
-   for type in $listallnc; do
+   for type in $listall_cnv; do
       count=$(ls pe*.${type}_${loop}.nc4 | wc -l)
       if [[ $count -gt 0 ]]; then
-         ${APRUN} ./nc_diag_cat.x -o ncdiag_${type}_${string}.nc4.${YYYYMMDDHH} pe*.${type}_${loop}.nc4
+         ${APRUN} ./nc_diag_cat.x -o diag_${type}_${string}.${YYYYMMDDHH}.nc4 pe*.${type}_${loop}.nc4
+         gzip diag_${type}_${string}.${YYYYMMDDHH}.nc4*
+         echo "diag_${type}_${string}.${YYYYMMDDHH}.nc4*" >> listcnv
+         numfile_cnv=`expr ${numfile_cnv} + 1`
+      fi
+   done
+
+   for type in $listall_rad; do
+      count=$(ls pe*.${type}_${loop}.nc4 | wc -l)
+      if [[ $count -gt 0 ]]; then
+         ${APRUN} ./nc_diag_cat.x -o diag_${type}_${string}.${YYYYMMDDHH}.nc4 pe*.${type}_${loop}.nc4
+         gzip diag_${type}_${string}.${YYYYMMDDHH}.nc4*
+         echo "diag_${type}_${string}.${YYYYMMDDHH}.nc4*" >> listrad
+         numfile_rad=`expr ${numfile_rad} + 1`
+      else
+         echo 'No diag_' ${type} 'exist'
       fi
    done
 fi
@@ -909,10 +930,21 @@ if [ ${DO_RADDA} == "TRUE" ]; then
   else
     spinup_or_prod_rrfs=prod
   fi
-  tar -cvzf rrfs.${spinup_or_prod_rrfs}.${YYYYMMDDHH}_radstat *diag*.${YYYYMMDDHH}
+  if [ ${numfile_cnv} -gt 0 ]; then
+     tar -cvzf rrfs.${spinup_or_prod_rrfs}.${YYYYMMDDHH}_cnvstat_nc `cat listcnv`
+     cp_vrfy ./rrfs.${spinup_or_prod_rrfs}.${YYYYMMDDHH}_cnvstat_nc  ${satbias_dir}/rrfs.${spinup_or_prod_rrfs}.${YYYYMMDDHH}_cnvstat
+  fi
+  if [ ${numfile_rad} -gt 0 ]; then
+     tar -cvzf rrfs.${spinup_or_prod_rrfs}.${YYYYMMDDHH}_radstat_nc `cat listrad`
+     cp_vrfy ./rrfs.${spinup_or_prod_rrfs}.${YYYYMMDDHH}_radstat_nc  ${satbias_dir}/rrfs.${spinup_or_prod_rrfs}.${YYYYMMDDHH}_radstat
+  fi
+  if [ ${numfile_rad_bin} -gt 0 ]; then
+     tar -cvzf rrfs.${spinup_or_prod_rrfs}.${YYYYMMDDHH}_radstat `cat listrad_bin`
+     cp_vrfy ./rrfs.${spinup_or_prod_rrfs}.${YYYYMMDDHH}_radstat  ${satbias_dir}/rrfs.${spinup_or_prod_rrfs}.${YYYYMMDDHH}_radstat
+  fi
+
   cp_vrfy ./satbias_out ${satbias_dir}/rrfs.${spinup_or_prod_rrfs}.${YYYYMMDDHH}_satbias
   cp_vrfy ./satbias_pc.out ${satbias_dir}/rrfs.${spinup_or_prod_rrfs}.${YYYYMMDDHH}_satbias_pc
-  cp_vrfy ./rrfs.${spinup_or_prod_rrfs}.${YYYYMMDDHH}_radstat  ${satbias_dir}/rrfs.${spinup_or_prod_rrfs}.${YYYYMMDDHH}_radstat
 fi
 
 #
