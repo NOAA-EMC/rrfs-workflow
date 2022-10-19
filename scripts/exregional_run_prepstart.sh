270999559f28666a3ce35f62617b0043311abff4
Ming Hu <Ming.Hu@noaa.gov>
2022-10-19 13:04:53 -0600

    Add smoke/dust cycle at 04/16 spinup cycle. (#438)
    Small bug fixes.

--- scripts/exregional_run_prepstart.sh
+++ scripts/exregional_run_prepstart.sh
@@ -495,6 +495,71 @@ fi
 
 #-----------------------------------------------------------------------
 #
+#  smoke/dust cycling
+#
+#-----------------------------------------------------------------------
+if [ "${DO_SMOKE_DUST}" = "TRUE" ] && [ ${cycle_type} == "spinup" ] ; then  # cycle smoke/dust fields
+  if [ ${HH} -eq 4 ] || [ ${HH} -eq 16 ] ; then
+
+# figure out which surface is available
+      surface_file_dir_name=fcst_fv3lam
+      bkpath_find="missing"
+      restart_prefix_find="missing"
+      if [ "${bkpath_find}" == "missing" ]; then
+          restart_prefix=$( date +%Y%m%d.%H0000. -d "${START_DATE}" )
+
+          offset_hours=${DA_CYCLE_INTERV}
+          YYYYMMDDHHmInterv=$( date +%Y%m%d%H -d "${START_DATE} ${offset_hours} hours ago" )
+          bkpath=${fg_root}/${YYYYMMDDHHmInterv}${SLASH_ENSMEM_SUBDIR}/${surface_file_dir_name}/RESTART  
+
+          n=${DA_CYCLE_INTERV}
+          while [[ $n -le 6 ]] ; do
+             if [ "${IO_LAYOUT_Y}" == "1" ]; then
+               checkfile=${bkpath}/${restart_prefix}fv_tracer.res.tile1.nc
+             else
+               checkfile=${bkpath}/${restart_prefix}fv_tracer.res.tile1.nc.0000
+             fi
+             if [ -r "${checkfile}" ] && [ "${bkpath_find}" == "missing" ]; then
+               bkpath_find=${bkpath}
+               restart_prefix_find=${restart_prefix}
+               print_info_msg "$VERBOSE" "Found ${checkfile}; Use it for smoke/dust cycle "
+               break
+             fi
+ 
+             n=$((n + ${DA_CYCLE_INTERV}))
+             offset_hours=${n}
+             YYYYMMDDHHmInterv=$( date +%Y%m%d%H -d "${START_DATE} ${offset_hours} hours ago" )
+             bkpath=${fg_root}/${YYYYMMDDHHmInterv}${SLASH_ENSMEM_SUBDIR}/${surface_file_dir_name}/RESTART  # cycling, use background from RESTART
+             print_info_msg "$VERBOSE" "Trying this path: ${bkpath}"
+          done
+      fi
+
+# cycle smoke/dust
+      rm -f cycle_smoke_dust.done
+      if [ "${bkpath_find}" == "missing" ]; then
+        print_info_msg "Warning: cannot find smoke/dust files from previous cycle"
+      else
+        if [ "${IO_LAYOUT_Y}" == "1" ]; then
+          checkfile=${bkpath_find}/${restart_prefix_find}fv_tracer.res.tile1.nc
+          if [ -r "${checkfile}" ]; then
+            ncks -A -v smoke,dust ${checkfile}  fv_tracer.res.tile1.nc
+          fi
+        else
+          for ii in ${list_iolayout}
+          do
+            iii=$(printf %4.4i $ii)
+            checkfile=${bkpath_find}/${restart_prefix_find}fv_tracer.res.tile1.nc.${iii}
+            if [ -r "${checkfile}" ]; then
+              ncks -A -v smoke,dust ${checkfile}  fv_tracer.res.tile1.nc.${iii}
+            fi
+          done
+        fi
+        echo "${YYYYMMDDHH}(${cycle_type}): cycle smoke/dust from ${checkfile} " >> ${EXPTDIR}/log.cycles
+      fi
+  fi
+fi
+#-----------------------------------------------------------------------
+#
 #  surface cycling
 #
 #-----------------------------------------------------------------------
