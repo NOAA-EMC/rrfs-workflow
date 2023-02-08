33fc1c7822b55ad95ef9d38a54e687e1cead6be2
Guoqing Ge <guoqing.ge@noaa.gov>
2023-02-08 08:28:42 -0700

    Update the RTMA config file (#482)
    and Minor bug fix on Init.sh

--- Init.sh
+++ Init.sh
@@ -6,7 +6,7 @@ utildir="$basedir"/ush/bash_utils
 # set the pre-commit hook
 echo -e "#!/bin/sh" > $basedir/.git/hooks/pre-commit
 chmod +x $basedir/.git/hooks/pre-commit
-echo -e "$utildir/preCommit">>$basedir/.git/hooks/pre-commit
+echo -e "ush/bash_utils/preCommit">>$basedir/.git/hooks/pre-commit
 
 $utildir/make_agent_link_for_fix
 touch $basedir/fix/INIT_DONE
