d2f37fdd83e293ccf331c66352017619156ccb9e
Ming Hu <Ming.Hu@noaa.gov>
2023-05-30 15:30:53 -0600

    updates from RRFS_A v0.5.2. (#531)

--- ush/set_rrfs_config_general.sh
+++ ush/set_rrfs_config_general.sh
@@ -117,11 +117,11 @@ START_TIME_LIGHTNINGNC="00:55:00"
 START_TIME_PROCSMOKE="00:55:00"
 
 if [[ ${PREDEF_GRID_NAME} == "RRFS_CONUS_3km" ]] ; then 
-  DT_ATMOS=60
+  DT_ATMOS=36
   ADDNL_OUTPUT_GRIDS=()
   TILE_LABELS="CONUS REGIONS"
   TILE_SETS="full NE,NC,NW,SE,SC,SW"
-  CCPP_PHYS_SUITE="FV3_HRRR_gf"
+  CCPP_PHYS_SUITE="FV3_HRRR"
 
   if [[ $MACHINE == "jet" ]] ; then
     if [[ -n $RESERVATION ]] ; then
@@ -335,8 +335,8 @@ if [[ ${PREDEF_GRID_NAME} == "RRFS_CONUS_13km" ]] ; then
 fi
 
 if [[ ${PREDEF_GRID_NAME} == "RRFS_NA_3km" ]] ; then 
-  DT_ATMOS=60
-  CCPP_PHYS_SUITE="FV3_HRRR_gf"
+  DT_ATMOS=36
+  CCPP_PHYS_SUITE="FV3_HRRR"
   ADDNL_OUTPUT_GRIDS=( "hrrr" "hrrrak" )
   TILE_LABELS="NA hrrr_regions1 hrrr_regions2 hrrr_tiles1 hrrr_tiles2 hrrr_tiles3 \
     hrrr_tiles4 hrrrak_tiles"
