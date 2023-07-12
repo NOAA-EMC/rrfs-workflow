523b85939e6acc6917253c70661aeae0febf2efc
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2023-07-12 11:21:32 +0000

    fix failure on run_post

--- ush/fix_rrfs_locations.sh
+++ ush/fix_rrfs_locations.sh
@@ -8,18 +8,9 @@ if [[ -d /lfs/h2 ]] ; then
 elif [[ -d /scratch1 ]] ; then
     PLATFORM=hera
     FIX_RRFS_LOCATION="/scratch2/BMC/rtrr/FIX_RRFS"
-elif [[ -d /carddata ]] ; then
-    PLATFORM=s4
-    FIX_RRFS_LOCATION="/to/do"
 elif [[ -d /jetmon ]] ; then
     PLATFORM=jet
     FIX_RRFS_LOCATION="/lfs4/BMC/nrtrr/FIX_RRFS"
-elif [[ -d /glade ]] ; then
-    PLATFORM=cheyenne
-    FIX_RRFS_LOCATION="/glade/p/ral/jntp/FIX_RRFS"
-elif [[ -d /sw/gaea ]] ; then
-    PLATFORM=gaea
-    FIX_RRFS_LOCATION="/to/do"
 elif [[ -d /work ]]; then
     PLATFORM=orion
     FIX_RRFS_LOCATION="/work/noaa/rtrr/FIX_RRFS"
