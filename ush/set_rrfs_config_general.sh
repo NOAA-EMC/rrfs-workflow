2f577389569c9f1d44d384d639e852c669a28b8f
chan-hoo <chan-hoo.jeon@noaa.gov>
2023-07-18 14:17:59 +0000

    revert change in config

--- ush/set_rrfs_config_general.sh
+++ ush/set_rrfs_config_general.sh
@@ -50,19 +50,12 @@ fi
 if [[ $MACHINE == "wcoss2" ]] ; then
   ACCOUNT=RRFS-DEV
   HPSS_ACCOUNT="RRFS-DEV"
-#  QUEUE_DEFAULT="pridev"
-#  QUEUE_ANALYSIS="pridev"
-#  QUEUE_FCST="pridev"
-#  QUEUE_HPSS="dev_transfer"
-#  QUEUE_PRDGEN="pridev"
-#  QUEUE_GRAPHICS="pridev"
-
-  QUEUE_DEFAULT="dev"
-  QUEUE_ANALYSIS="dev"
-  QUEUE_FCST="dev"
+  QUEUE_DEFAULT="pridev"
+  QUEUE_ANALYSIS="pridev"
+  QUEUE_FCST="pridev"
   QUEUE_HPSS="dev_transfer"
-  QUEUE_PRDGEN="dev"
-  QUEUE_GRAPHICS="dev"
+  QUEUE_PRDGEN="pridev"
+  QUEUE_GRAPHICS="pridev"
 fi
 
 VERBOSE="TRUE"
