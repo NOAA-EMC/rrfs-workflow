36d175d8ce30c8f72bd2d3ee94e4338c9735ff66
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2023-08-02 19:16:14 +0000

    change bash to python script

--- scripts/exregional_run_fcst.sh
+++ scripts/exregional_run_fcst.sh
@@ -12,15 +12,6 @@
 #
 #-----------------------------------------------------------------------
 #
-# Source other necessary files.
-#
-#-----------------------------------------------------------------------
-#
-. $USHdir/create_model_configure_file.sh
-. $USHdir/create_diag_table_file.sh
-#
-#-----------------------------------------------------------------------
-#
 # Save current shell options (in a global array).  Then set new options
 # for this script/function.
 #
@@ -515,30 +506,40 @@ fi
 #
 #-----------------------------------------------------------------------
 #
-create_model_configure_file \
-  cdate="$cdate" \
-  cycle_type="$cycle_type" \
-  cycle_subtype="$cycle_subtype" \
-  run_dir="${run_dir}" \
-  nthreads=${OMP_NUM_THREADS:-1} \
-  restart_hrs="${RESTART_HRS}" || print_err_msg_exit "\
-Call to function to create a model configuration file for the current
-cycle's (cdate) run directory (run_dir) failed:
-  cdate = \"${cdate}\"
-  run_dir = \"${run_dir}\""
+python3 $USHdir/create_model_configure_file.py \
+  --path-to-defns ${GLOBAL_VAR_DEFNS_FP} \
+  --cdate "${cdate}" \
+  --cycle_type "${cycle_type}" \
+  --cycle_subtype "${cycle_subtype}" \
+  --run-dir "${run_dir}" \
+  --fhrot "${FHROT}" \
+  --nthreads "${OMP_NUM_THREADS}" \
+  --restart_hrs="${RESTART_HRS}"
+export err=$?
+if [ $err -ne 0 ]; then
+  message_txt="Call to function to create the model_configure file for
+the current cycle's (cdate) run directory (DATA) failed:
+  DATA = \"${run_dir}\""
+  err_exit "${message_txt}"
+fi
 #
 #-----------------------------------------------------------------------
 #
-# Call the function that creates the model configuration file within each
+# Call the function that creates the diag_table file within each
 # cycle directory.
 #
 #-----------------------------------------------------------------------
 #
-create_diag_table_file \
-  run_dir="${run_dir}" || print_err_msg_exit "\
-  Call to function to create a diag table file for the current.
-cycle's (cdate) run directory (run_dir) failed:
-  run_dir = \"${run_dir}\""
+python3 $USHdir/create_diag_table_file.py \
+  --path-to-defns ${GLOBAL_VAR_DEFNS_FP} \
+  --run-dir ${run_dir}
+export err=$?
+if [ $err -ne 0 ]; then
+  message_txt="Call to function to create the diag_table file for
+the current cycle's (cdate) run directory (DATA) failed:
+  DATA = \"${run_dir}\""
+  err_exit "${message_txt}"
+fi
 #
 #-----------------------------------------------------------------------
 #
@@ -552,7 +553,7 @@ python3 $USHdir/create_nems_configure_file.py \
   --run-dir ${run_dir} 
 export err=$?
 if [ $err -ne 0 ]; then
-  message_txt="Call to function to create a NEMS configuration file for
+  message_txt="Call to function to create the NEMS configuration file for
 the current cycle's (cdate) run directory (DATA) failed:
   DATA = \"${run_dir}\""
   err_exit "${message_txt}"
---
+++ ush/create_model_configure_file.py
@@ -0,0 +1,361 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import argparse
+import unittest
+from datetime import datetime
+from textwrap import dedent
+
+from python_utils import (
+    import_vars,
+    set_env_var,
+    print_input_args,
+    str_to_type,
+    print_info_msg,
+    print_err_msg_exit,
+    lowercase,
+    cfg_to_yaml_str,
+    load_shell_config,
+    flatten_dict,
+)
+
+from fill_jinja_template import fill_jinja_template
+
+
+def create_model_configure_file(
+    cdate, cycle_type, cycle_subtype, run_dir, fhrot, nthreads, restart_hrs
+):
+    """Creates a model configuration file in the specified
+    run directory
+
+    Args:
+        cdate: cycle date
+        cycle_type: type of cycle
+        cycle_subtype: sub-type of cycle
+        run_dir: run directory
+        fhrot: forecast hour at restart
+        nthreads: omp_num_threads
+        restart_hrs: restart hours
+    Returns:
+        Boolean
+    """
+
+    print_input_args(locals())
+
+    # import all environment variables
+    import_vars()
+
+    #
+    # -----------------------------------------------------------------------
+    #
+    # Create a model configuration file in the specified run directory.
+    #
+    # -----------------------------------------------------------------------
+    #
+    print_info_msg(
+        f"""
+        Creating a model configuration file ('{MODEL_CONFIG_FN}') in the specified
+        run directory (run_dir):
+          run_dir = '{run_dir}'""",
+        verbose=VERBOSE,
+    )
+    #
+    # Extract from cdate the starting year, month, day, and hour of the forecast.
+    #
+    yyyy = cdate.year
+    mm = cdate.month
+    dd = cdate.day
+    hh = cdate.hour
+    #
+    # Set parameters in the model configure file.
+    #
+    dot_quilting_dot=f".{lowercase(str(QUILTING))}."
+    dot_write_dopost=f".{lowercase(str(WRITE_DOPOST))}."
+    #
+    # Decide the forecast length for this cycle
+    #
+    if FCST_LEN_HRS_CYCLES == None:
+        FCST_LEN_HRS_thiscycle = FCST_LEN_HRS
+    else:
+        num_fhrs = len(FCST_LEN_HRS_CYCLES)
+        ihh = int(hh)
+        if num_fhrs > ihh:
+            FCST_LEN_HRS_thiscycle = FCST_LEN_HRS_CYCLES[ihh]
+        else:
+            FCST_LEN_HRS_thiscycle = FCST_LEN_HRS
+
+    print_info_msg(
+        f"""
+        The forecast length for cycle ('{hh}') is ('{FCST_LEN_HRS_thiscycle}').
+        """, verbose=VERBOSE,
+    )
+
+    if cycle_type == "spinup":
+        FCST_LEN_HRS_thiscycle = FCST_LEN_HRS_SPINUP
+        if cycle_subtype == "ensinit":
+            for cyc_start in CYCL_HRS_SPINSTART:
+                if hh == cyc_start:
+                    FCST_LEN_HRS_thiscycle = "{:0.5f}".format(DT_ATMOS/3600)
+                    NSOUT = 1
+                    RESTART_INTERVAL = 0
+                    print_info_msg(f"""
+        "DT_ATMOS ('{DT_ATMOS}') 
+         FCST_LEN_HRS_thiscycle ('{FCST_LEN_HRS_thiscycle}')
+         NSOUT ('{NSOUT}')
+         RESTART_INTERVAL ('{RESTART_INTERVAL}')
+                    """, verbose=VERBOSE)
+
+    #
+    # -----------------------------------------------------------------------
+    #
+    # Create a multiline variable that consists of a yaml-compliant string
+    # specifying the values that the jinja variables in the template
+    # model_configure file should be set to.
+    #
+    # -----------------------------------------------------------------------
+    #
+    settings = {
+        "start_year": yyyy,
+        "start_month": mm,
+        "start_day": dd,
+        "start_hour": hh,
+        "nhours_fcst": FCST_LEN_HRS_thiscycle,
+        "fhrot": fhrot,
+        "dt_atmos": DT_ATMOS,
+        "restart_interval": RESTART_INTERVAL,
+        "write_dopost": dot_write_dopost,
+        "quilting": dot_quilting_dot,
+        "output_grid": WRTCMP_output_grid,
+        "nsout": NSOUT
+    }
+    #
+    # If the write-component is to be used, then specify a set of computational
+    # parameters and a set of grid parameters.  The latter depends on the type
+    # (coordinate system) of the grid that the write-component will be using.
+    #
+    if QUILTING:
+        settings.update(
+            {
+                "write_groups": WRTCMP_write_groups,
+                "write_tasks_per_group": WRTCMP_write_tasks_per_group,
+                "cen_lon": WRTCMP_cen_lon,
+                "cen_lat": WRTCMP_cen_lat,
+                "lon1": WRTCMP_lon_lwr_left,
+                "lat1": WRTCMP_lat_lwr_left,
+            }
+        )
+
+        if WRTCMP_output_grid == "lambert_conformal":
+            settings.update(
+                {
+                    "stdlat1": WRTCMP_stdlat1,
+                    "stdlat2": WRTCMP_stdlat2,
+                    "nx": WRTCMP_nx,
+                    "ny": WRTCMP_ny,
+                    "dx": WRTCMP_dx,
+                    "dy": WRTCMP_dy,
+                    "lon2": "",
+                    "lat2": "",
+                    "dlon": "",
+                    "dlat": "",
+                }
+            )
+        elif (
+            WRTCMP_output_grid == "regional_latlon"
+            or WRTCMP_output_grid == "rotated_latlon"
+        ):
+            settings.update(
+                {
+                    "lon2": WRTCMP_lon_upr_rght,
+                    "lat2": WRTCMP_lat_upr_rght,
+                    "dlon": WRTCMP_dlon,
+                    "dlat": WRTCMP_dlat,
+                    "stdlat1": "",
+                    "stdlat2": "",
+                    "nx": "",
+                    "ny": "",
+                    "dx": "",
+                    "dy": "",
+                }
+            )
+    #
+    # If sub_hourly_post is set to "TRUE", then the forecast model must be
+    # directed to generate output files on a sub-hourly interval.  Do this
+    # by specifying the output interval in the model configuration file
+    # (MODEL_CONFIG_FN) in units of number of forecat model time steps (nsout).
+    # nsout is calculated using the user-specified output time interval
+    # dt_subhourly_post_mnts (in units of minutes) and the forecast model's
+    # main time step dt_atmos (in units of seconds).  Note that nsout is
+    # guaranteed to be an integer because the experiment generation scripts
+    # require that dt_subhourly_post_mnts (after conversion to seconds) be
+    # evenly divisible by dt_atmos.  Also, in this case, the variable output_fh
+    # [which specifies the output interval in hours;
+    # see the jinja model_config template file] is set to 0, although this
+    # doesn't matter because any positive of nsout will override output_fh.
+    #
+    # If sub_hourly_post is set to "FALSE", then the workflow is hard-coded
+    # (in the jinja model_config template file) to direct the forecast model
+    # to output files every hour.  This is done by setting (1) output_fh to 1
+    # here, and (2) nsout to -1 here which turns off output by time step interval.
+    #
+    # Note that the approach used here of separating how hourly and subhourly
+    # output is handled should be changed/generalized/simplified such that
+    # the user should only need to specify the output time interval (there
+    # should be no need to specify a flag like sub_hourly_post); the workflow
+    # should then be able to direct the model to output files with that time
+    # interval and to direct the post-processor to process those files
+    # regardless of whether that output time interval is larger than, equal
+    # to, or smaller than one hour.
+    #
+    if sub_hourly_post:
+        nsout = (dt_subhourly_post_mnts * 60) // dt_atmos
+        output_fh = 0
+    else:
+        output_fh = 1
+        nsout = -1
+
+    settings.update({"output_fh": output_fh, "nsout": nsout})
+
+    settings_str = cfg_to_yaml_str(settings)
+
+    print_info_msg(
+        dedent(
+            f"""
+            The variable 'settings' specifying values to be used in the '{MODEL_CONFIG_FN}'
+            file has been set as follows:\n
+            settings =\n\n"""
+        )
+        + settings_str,
+        verbose=VERBOSE,
+    )
+    #
+    # -----------------------------------------------------------------------
+    #
+    # Call a python script to generate the experiment's actual MODEL_CONFIG_FN
+    # file from the template file.
+    #
+    # -----------------------------------------------------------------------
+    #
+    model_config_fp = os.path.join(run_dir, MODEL_CONFIG_FN)
+
+    try:
+        fill_jinja_template(
+            [
+                "-q",
+                "-u",
+                settings_str,
+                "-t",
+                MODEL_CONFIG_TMPL_FP,
+                "-o",
+                model_config_fp,
+            ]
+        )
+    except:
+        print_err_msg_exit(
+            dedent(
+                f"""
+                Call to python script fill_jinja_template.py to create a '{MODEL_CONFIG_FN}'
+                file from a jinja2 template failed.  Parameters passed to this script are:
+                  Full path to template model config file:
+                    MODEL_CONFIG_TMPL_FP = '{MODEL_CONFIG_TMPL_FP}'
+                  Full path to output model config file:
+                    model_config_fp = '{model_config_fp}'
+                  Namelist settings specified on command line:\n
+                    settings =\n\n"""
+            )
+            + settings_str
+        )
+        return False
+
+    exit()
+
+    return True
+
+
+def parse_args(argv):
+    """Parse command line arguments"""
+    parser = argparse.ArgumentParser(description="Creates model configuration file.")
+
+    parser.add_argument(
+        "-d", 
+        "--run-dir", 
+        dest="run_dir", 
+        required=True, 
+        help="Run directory."
+    )
+
+    parser.add_argument(
+        "-c",
+        "--cdate",
+        dest="cdate",
+        required=True,
+        help="Date string in YYYYMMDD format.",
+    )
+
+    parser.add_argument(
+        "-t",
+        "--cycle_type",
+        dest="cycle_type",
+        required=True,
+        help="Type of cycle.",
+    )
+
+    parser.add_argument(
+        "-s",
+        "--cycle_subtype",
+        dest="cycle_subtype",
+        required=True,
+        help="Sub-type of cycle.",
+    )
+
+    parser.add_argument(
+        "-f",
+        "--fhrot",
+        dest="fhrot",
+        required=True,
+        help="Forecast hour at restart.",
+    )
+
+    parser.add_argument(
+        "-n",
+        "--nthreads",
+        dest="nthreads",
+        required=True,
+        help="OMP_NUM_THREADS.",
+    )
+
+    parser.add_argument(
+        "-r",
+        "--restart_hrs",
+        dest="restart_hrs",
+        required=True,
+        help="Restart hours.",
+    )
+
+    parser.add_argument(
+        "-p",
+        "--path-to-defns",
+        dest="path_to_defns",
+        required=True,
+        help="Path to var_defns file.",
+    )
+
+    return parser.parse_args(argv)
+
+
+if __name__ == "__main__":
+    args = parse_args(sys.argv[1:])
+    cfg = load_shell_config(args.path_to_defns)
+    cfg = flatten_dict(cfg)
+    import_vars(dictionary=cfg)
+    create_model_configure_file(
+        run_dir=args.run_dir,
+        cdate=str_to_type(args.cdate),
+        cycle_type=str_to_type(args.cycle_type),
+        cycle_subtype=str_to_type(args.cycle_subtype),
+        fhrot=str_to_type(args.fhrot),
+        nthreads=str_to_type(args.nthreads),
+        restart_hrs=str_to_type(args.restart_hrs),
+    )
+
