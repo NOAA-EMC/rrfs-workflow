84ac204409890ded44698379e8f5d7a5cce1899c
chan-hoo <chan-hoo.jeon@noaa.gov>
2023-10-23 18:10:43 +0000

    add missing module for wcoss2

--- modulefiles/tasks/wcoss2/run_anal_gsi.local.lua
+++ modulefiles/tasks/wcoss2/run_anal_gsi.local.lua
@@ -17,6 +17,7 @@ load(pathJoin("nemsio", os.getenv("nemsio_ver")))
 load(pathJoin("wrf_io", os.getenv("wrf_io_ver")))
 load(pathJoin("ncio", os.getenv("ncio_ver")))
 load(pathJoin("ncdiag", os.getenv("ncdiag_ver")))
+load(pathJoin("libjpeg", os.getenv("libjpeg_ver")))
 
 load(pathJoin("udunits", os.getenv("udunits_ver")))
 load(pathJoin("gsl", os.getenv("gsl_ver")))
