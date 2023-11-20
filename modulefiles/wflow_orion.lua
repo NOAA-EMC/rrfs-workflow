d9e1360de2c9202184c351c4e96603efb8a813c6
Chan-Hoo.Jeon-NOAA <60152248+chan-hoo@users.noreply.github.com>
2023-11-20 15:50:08 -0500

    Update module files to fix failure on Orion (#103)
    * update modulefile for orion
    
    * update wflow_orion
    
    * update python_srw for orion
    
    * update path to fix files on Orion
    
    * fix err_exit for Orion
    
    * increase wtime for run_fcst on Orion
    
    ---------
    
    Co-authored-by: guoqing.ge <Guoqing.Ge@noaa.gov>
    Co-authored-by: MatthewPyle-NOAA <48285220+MatthewPyle-NOAA@users.noreply.github.com>

--- modulefiles/wflow_orion.lua
+++ modulefiles/wflow_orion.lua
@@ -9,14 +9,14 @@ load("contrib")
 load("rocoto")
 load("wget")
 
-prepend_path("MODULEPATH","/work/noaa/epic-ps/role-epic-ps/hpc-stack/libs/intel-2022.1.2/modulefiles/stack")
+prepend_path("MODULEPATH","/work/noaa/epic/role-epic/contrib/orion/hpc-stack/intel-2022.1.2/modulefiles/stack")
 load(pathJoin("hpc", os.getenv("hpc_ver") or "1.2.0"))
 load(pathJoin("hpc-intel", os.getenv("hpc_intel_ver") or "2022.1.2"))
 load(pathJoin("hpc-impi", os.getenv("hpc_impi_ver") or "2022.1.2"))
 load(pathJoin("crtm", os.getenv("crtm_ver") or "2.4.0"))
 
 unload("python")
-append_path("MODULEPATH","/work/noaa/epic-ps/role-epic-ps/miniconda3/modulefiles")
+append_path("MODULEPATH","/work/noaa/epic/role-epic/contrib/orion/miniconda3/modulefiles")
 load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.12.0"))
 
 if mode() == "load" then
