21d8933360e9333d2f4e0f9b8d7fb42fef3a8533
matthew pyle <Matthew.Pyle@noaa.gov>
2023-12-20 19:53:31 +0000

    Pushes module task files for run_bufrsnd from being a local change into the workflow.

--- modulefiles/tasks/wcoss2/run_bufrsnd.local.lua
+++ modulefiles/tasks/wcoss2/run_bufrsnd.local.lua
@@ -1,4 +1,17 @@
 load("python_srw")
 
-load(pathJoin("gempak", os.getenv("gempak_ver")))
+unload("PrgEnv-intel")
+unload("netcdf")
+
+load(pathJoin("intel", os.getenv("intel_ver")))
+load(pathJoin("PrgEnv-intel", os.getenv("PrgEnv_intel_ver")))
+load(pathJoin("craype", os.getenv("craype_ver")))
+load(pathJoin("cray-mpich", os.getenv("cray_mpich_ver")))
 load(pathJoin("cray-pals", os.getenv("cray_pals_ver")))
+
+prepend_path("MODULEPATH", os.getenv("modulepath_compiler"))
+prepend_path("MODULEPATH", os.getenv("modulepath_mpi"))
+
+load(pathJoin("hdf5", os.getenv("hdf5_ver")))
+load(pathJoin("netcdf", os.getenv("netcdf_ver")))
+load(pathJoin("gempak", os.getenv("gempak_ver")))
