a9313d1c91e230968349b02e0d681769c898db5b
ShawnMurdzek-NOAA <113145197+ShawnMurdzek-NOAA@users.noreply.github.com>
2023-12-20 16:07:15 -0700

    I_MPI_EXTRA_FILESYSTEM=ON and load wget after intel

--- modulefiles/wflow_hercules.lua
+++ modulefiles/wflow_hercules.lua
@@ -7,12 +7,13 @@ whatis([===[Loads libraries needed for running RRFS workflow on Hercules ]===])
 
 load("contrib")
 load("rocoto")
-load("wget")
 
 prepend_path("MODULEPATH","/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.5.0/envs/unified-env/install/modulefiles/Core")
 load(pathJoin("stack-intel", os.getenv("stack_intel_ver") or "2021.9.0"))
 load(pathJoin("stack-intel-oneapi-mpi", os.getenv("stack_impi_ver") or "2021.9.0"))
 load(pathJoin("crtm", os.getenv("crtm_ver") or "2.4.0"))
+
+load("wget")
 
 unload("python")
 append_path("MODULEPATH","/work/noaa/epic/role-epic/contrib/hercules/miniconda3/modulefiles")
