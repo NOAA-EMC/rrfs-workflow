9cb9caa56c435ed6be4885dad0bddccc19512f8d
Ming Hu <Ming.Hu@noaa.gov>
2024-04-03 09:35:48 -0400

    Update Hera compile and run environments for Rocky 8 (#302)
    * Update Hera compile and run environments for Rocky 8.
    * remove unload("python/3.10.8") from build_hera_intel.lua

--- modulefiles/build_hera_intel.lua
+++ modulefiles/build_hera_intel.lua
@@ -5,7 +5,7 @@ the NOAA RDHPC machine Hera using Intel-2022.1.2
 
 whatis([===[Loads libraries needed for building the RRFS workflow on Hera ]===])
 
-prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.5.1/envs/gsi-addon/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.5.1/envs/gsi-addon-env-rocky8/install/modulefiles/Core")
 load(pathJoin("stack-intel", os.getenv("stack_intel_ver") or "2021.5.0"))
 load(pathJoin("stack-intel-oneapi-mpi", os.getenv("stack_impi_ver") or "2021.5.1"))
 load(pathJoin("cmake", os.getenv("cmake_ver") or "3.23.1"))
@@ -15,8 +15,6 @@ load(pathJoin("wgrib2", os.getenv("wgrib2_ver") or "2.0.8"))
 
 prepend_path("MODULEPATH", "/scratch2/BMC/rtrr/gge/lua")
 load("prod_util/2.0.15")
-
-unload("python/3.10.8")
 
 setenv("CMAKE_C_COMPILER","mpiicc")
 setenv("CMAKE_CXX_COMPILER","mpiicpc")
--- modulefiles/wflow_hera.lua
+++ modulefiles/wflow_hera.lua
@@ -7,7 +7,7 @@ whatis([===[Loads libraries needed for running the RRFS workflow on Hera ]===])
 
 load("rocoto")
 
-prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.5.1/envs/gsi-addon/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.5.1/envs/gsi-addon-env-rocky8/install/modulefiles/Core")
 load(pathJoin("stack-intel", os.getenv("stack_intel_ver") or "2021.5.0"))
 load(pathJoin("stack-intel-oneapi-mpi", os.getenv("stack_impi_ver") or "2021.5.1"))
 load(pathJoin("crtm", os.getenv("crtm_ver") or "2.4.0"))
