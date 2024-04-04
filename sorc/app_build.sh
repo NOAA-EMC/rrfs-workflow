0ed88488c77370070a48a0efeb0f68cb7b2cfdde
Ming.Hu <Ming.Hu@noaa.gov>
2024-04-04 03:34:28 +0000

    Update compile system to separate GTG and IFI compiling option. So we can compile GTG or IFI or both.

--- sorc/app_build.sh
+++ sorc/app_build.sh
@@ -26,7 +26,9 @@ OPTIONS
       disable ufs-weather-model options; delimited with ','
       (e.g. 32BIT | INLINE_POST | UFS_GOCART | MOM6 | CICE6 | WW3 | CMEPS)
   --nogtg
-      build without GTG and libIFI (default is true, this option turns it off)
+      build without GTG (default is true, this option turns it off)
+  --noifi
+      build without IFI (default is true, this option turns it off)
   --extrn
       check out external components
   --continue
@@ -82,6 +84,7 @@ Settings:
   DISABLE_OPTIONS=${DISABLE_OPTIONS}
   EXTRN=${EXTRN}
   NOGTG=${NOGTG}
+  NOIFI=${NOIFI}
   REMOVE=${REMOVE}
   CONTINUE=${CONTINUE}
   BUILD_TYPE=${BUILD_TYPE}
@@ -121,6 +124,7 @@ BUILD_TYPE="Release"
 BUILD_JOBS=4
 EXTRN=false
 NOGTG=false
+NOIFI=false
 REMOVE=false
 CONTINUE=false
 VERBOSE=false
@@ -166,6 +170,7 @@ while :; do
     --extrn) EXTRN=true ;;
     --extrn=?*|--extrn=) usage_error "$1 argument ignored." ;;
     --nogtg) NOGTG=true ;;
+    --noifi) NOIFI=true ;;
     --remove) REMOVE=true ;;
     --remove=?*|--remove=) usage_error "$1 argument ignored." ;;
     --continue) CONTINUE=true ;;
@@ -239,9 +244,13 @@ MACHINE="${PLATFORM}"
 printf "PLATFORM(MACHINE)=${PLATFORM}\n" >&2
 
 # Determine whether to build GTG and IFI with UPP
+BUILD_GTG="on"
+BUILD_IFI="on"
 if [ "${NOGTG}" = true ]; then
-  cd ${SORC_DIR}
-  cp ./CMakeLists_withoutgtg.txt ./CMakeLists.txt
+  BUILD_GTG="off"
+fi
+if [ "${NOIFI}" = true ]; then
+  BUILD_IFI="off"
 fi
 
 # check out external components specified in External.cfg
@@ -401,7 +410,9 @@ CMAKE_SETTINGS="\
  -DBUILD_GSI=${BUILD_GSI}\
  -DBUILD_RRFS_UTILS=${BUILD_RRFS_UTILS}\
  -DBUILD_NEXUS=${BUILD_NEXUS}\
- -DBUILD_AQM_UTILS=${BUILD_AQM_UTILS}"
+ -DBUILD_AQM_UTILS=${BUILD_AQM_UTILS}\
+ -DBUILD_IFI=${BUILD_IFI}\
+ -DBUILD_GTG=${BUILD_GTG}"
 
 if [ ! -z "${APPLICATION}" ]; then
   CMAKE_SETTINGS="${CMAKE_SETTINGS} -DAPP=${APPLICATION}"
