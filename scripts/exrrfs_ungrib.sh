5fb34d667f143698f8b6d9a2453a071b77eaa934
guoqing.ge <Guoqing.Ge@noaa.gov>
2024-09-03 08:32:36 -0600

    clean up exrrfs_ungrib.sh per the review feedback

--- scripts/exrrfs_ungrib.sh
+++ scripts/exrrfs_ungrib.sh
@@ -25,8 +25,6 @@ FHRin=$(( 10#${FHR}+10#${offset} )) #FHR for input external data
 cd ${DATA}
 ${cpreq} ${FIXrrfs}/ungrib/Vtable.${prefix} Vtable
 #
-# preprocess grib2 files if it is RAP or RRFS
-WGRIB2=/apps/wgrib2/2.0.8/intel/18.0.5.274/bin/wgrib2
 if [[ "${prefix}" == "GFS" ]]; then
   fstr=$(printf %03d ${FHRin})
   filea=${COMINgfs}/gfs.${CDATEin:0:8}/${CDATEin:8:2}/gfs.t${CDATEin:8:2}z.pgrb2.0p25.f${fstr}
@@ -44,39 +42,13 @@ elif [[ "${prefix}" == "GEFS" ]]; then
 elif [[ "${prefix}" == "RAP" ]]; then
   fstr=$(printf %02d ${FHRin})
   GRIBFILE=${COMINrap}/rap.${CDATEin:0:8}/rap.t${CDATEin:8:2}z.wrfnatf${fstr}.grib2
-  # Interpolate to Lambert conformal grid
-  if true; then #gge.debug need to confirm when to do an interpolation for RAP and RRFS
-    ln -snf ${GRIBFILE} GRIBFILE.AAA
-  else
-    grid_specs_20km="lambert:-97.5:38.5 -133.174:449:20000.0 5.47114:299:20000.0"
-    ${WGRIB2} ${GRIBFILE} -set_bitmap 1 -set_grib_type c3 -new_grid_winds grid \
-           -new_grid_vectors "UGRD:VGRD:USTM:VSTM:VUCSH:VVCSH"               \
-           -new_grid_interpolation neighbor                                  \
-           -new_grid ${grid_specs_20km} tmp.grib2
-    # Merge vector field records
-    ${WGRIB2} tmp.grib2 -new_grid_vectors "UGRD:VGRD:USTM:VSTM:VUCSH:VVCSH" -submsg_uv 20km_grid.grib2
-    ln -snf 20km_grid.grib2 GRIBFILE.AAA
-  fi #skip preprocessing
+  ${cpreq} ${GRIBFILE} GRIBFILE.AAA
 
 elif [[ "${prefix}" == "RRFS" ]]; then
   fstr=$(printf %02d ${FHRin})
   GRIBFILE=${COMINrrfs1}/rrfs_a.${CDATEin:0:8}/${CDATEin:8:2}/rrfs.t${CDATEin:8:2}z.natlve.f${fstr}.grib2
-  # variation on the 130 grid at 3 km
-  grid_specs="lambert:266:25.000000 234.862000:2000:3000.000000 17.281000:1480:3000.000000"
-  ${WGRIB2} ${GRIBFILE} -set_bitmap 1 -set_grib_type c3 -new_grid_winds grid \
-	 -new_grid_vectors "UGRD:VGRD:USTM:VSTM:VUCSH:VVCSH"               \
-	 -new_grid_interpolation bilinear \
-	 -if "`cat ${FIXrrfs}/ungrib/budget_fields.txt`" -new_grid_interpolation budget -fi \
-	 -if "`cat ${FIXrrfs}/ungrib/neighbor_fields.txt`" -new_grid_interpolation neighbor -fi \
-	 -new_grid ${grid_specs} tmp.grib2
-  # Merge vector field records
-  ${WGRIB2} tmp.grib2 -new_grid_vectors "UGRD:VGRD:USTM:VSTM:VUCSH:VVCSH" -submsg_uv tmp2.grib2
-  if [[ -s tmp2.grib2 ]]; then
-    ln -snf tmp2.grib2 GRIBFILE.AAA
-  else
-    echo "tmp2.grib2 not created; exiting"
-    err_exit
-  fi
+  ${cpreq} ${GRIBFILE} GRIBFILE.AAA
+
 else
   echo "ungrib PREFIX=${prefix} not supported"
   err_exit
