e8a3e2539736266e30a609f50e8592de670cb585
Edward Hartnett <edward.hartnett@noaa.gov>
2024-09-10 06:16:38 -0600

    working on test

--- ush/python_utils/test_misc.py
+++ ush/python_utils/test_misc.py
@@ -10,5 +10,6 @@ class TestMisc:
         assert lowercase('S') == 's'
 
     def test_more(self):
+        print("howdy!\n")
         print(find_pattern_in_str('.', 's'))
-        assert find_pattern_in_str('.', 's')
+        assert !find_pattern_in_str('.', 's')
