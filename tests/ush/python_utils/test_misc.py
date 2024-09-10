7579076eaf39fc2a64d7366ae7f6fc7744567eaf
matthew pyle <Matthew.Pyle@noaa.gov>
2024-09-10 15:54:47 +0000

    Adds #478 changes from dev-sci, but have a question about the location of the .toml file.

---
+++ tests/ush/python_utils/test_misc.py
@@ -0,0 +1,25 @@
+
+from ush.python_utils.misc import *
+
+class TestMisc:
+    """Test the misc.py functions."""            
+
+    def test_uc(self):
+        """Test the uppercase() function."""        
+        assert uppercase('s') == 'S'
+
+    def test_lc(self):
+        """Test the lowercase() function."""        
+        assert lowercase('S') == 's'
+
+    def test_find_pattern_in_str(self):
+        """Test the find_pattern_in_str() function."""        
+        assert not find_pattern_in_str('.', 's')
+
+    def test_find_pattern_in_fike(self):
+        """Test the find_pattern_in_file() function."""        
+        f = open("test_misc.txt", "w")  
+        f.write("Hello World from " + f.name) 
+        f.close()
+        assert not find_pattern_in_file('H', "test_misc.txt")
+
