10a386a182967c25289c530b31f98e928364c996
benjamin blake <benjamin.blake@dlogin03.dogwood.wcoss2.ncep.noaa.gov>
2024-09-17 11:23:29 +0000

    Remove python_srw module

--- modulefiles/tasks/wcoss2/recenter.local.lua
+++ modulefiles/tasks/wcoss2/recenter.local.lua
@@ -1,11 +1,11 @@
-load("python_srw")
-
 unload("PrgEnv-intel")
 unload("netcdf")
 
 load(pathJoin("PrgEnv-intel", os.getenv("PrgEnv_intel_ver")))
 load(pathJoin("intel", os.getenv("intel_ver")))
 load(pathJoin("craype", os.getenv("craype_ver")))
+load(pathJoin("python", os.getenv("python_ver")))
+load(pathJoin("prod_util", os.getenv("prod_util_ver")))
 load(pathJoin("cray-mpich", os.getenv("cray_mpich_ver")))
 load(pathJoin("cray-pals", os.getenv("cray_pals_ver")))
 
