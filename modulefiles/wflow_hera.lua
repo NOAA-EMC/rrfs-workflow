5efa2a1a6af07e09c21658ddb76d65f8558e5040
MatthewPyle-NOAA <48285220+MatthewPyle-NOAA@users.noreply.github.com>
2024-09-19 12:23:55 -0400

    Updates wflow_hera on production branch as well. (#494)

--- modulefiles/wflow_hera.lua
+++ modulefiles/wflow_hera.lua
@@ -12,11 +12,11 @@ load(pathJoin("stack-intel", os.getenv("stack_intel_ver") or "2021.5.0"))
 load(pathJoin("stack-intel-oneapi-mpi", os.getenv("stack_impi_ver") or "2021.5.1"))
 load(pathJoin("crtm", os.getenv("crtm_ver") or "2.4.0"))
 
-prepend_path("MODULEPATH","/scratch1/NCEPDEV/nems/role.epic/miniconda3/modulefiles")
+prepend_path("MODULEPATH","/contrib/miniconda3/modulefiles")
 load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.12.0"))
 
 if mode() == "load" then
    LmodMsgRaw([===[Please do the following to activate conda:
-       > conda activate workflow_tools
+       > conda activate regional_workflow
 ]===])
 end
