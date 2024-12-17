0a55be0b68d31e92fbc5d1208dd7a54a05d943fd
chunhuazhou <chunhua.zhou@noaa.gov>
2024-12-17 20:47:41 +0000

    Changes for ensemble forecast

--- workflow/rocoto_funcs/fcst.py
+++ workflow/rocoto_funcs/fcst.py
@@ -5,40 +5,10 @@ from rocoto_funcs.base import xml_task, source, get_cascade_env
 ### begin of fcst --------------------------------------------------------
 def fcst(xmlFile, expdir, do_ensemble=False):
   # Task-specific EnVars beyond the task_common_vars
-  dcTaskEnv={}
-  if not do_ensemble:
-    metatask=False
-    meta_id=''
-    task_id='fcst'
-    cycledefs='prod'
-    hrs=os.getenv('PROD_BGN_AT_HRS', '3 15')
-    fcst_len_hrs_cycls=os.getenv('FCST_LEN_HRS_CYCLES', '03 03')
-    meta_bgn=""
-    meta_end=""
-    RUN='rrfs'
-    ensindexstr=""
-    ensstr=""
-  else:
-    metatask=True
-    meta_id='fcst'
-    task_id=f'{meta_id}_m#ens_index#'
-    cycledefs='ens_prod'
-    dcTaskEnv['ENS_INDEX']="#ens_index#"
-    hrs=os.getenv('ENS_PROD_BGN_AT_HRS', '3 15')
-    meta_bgn=""
-    meta_end=""
-    ens_size=int(os.getenv('ENS_SIZE','2'))
-    ens_indices=''.join(f'{i:03d} ' for i in range(1,int(ens_size)+1)).strip()
-    meta_bgn=f'''
-<metatask name="ens_{meta_id}">
-<var name="ens_index">{ens_indices}</var>'''
-    meta_end=f'\
-</metatask>\n'
-    RUN='ens'
-    ensindexstr="_m#ens_index#"
-    ensstr="ens_"
-
-  # Task-specific EnVars beyond the task_common_vars
+  meta_id='fcst'
+  cycledefs='prod'
+  hrs=os.getenv('PROD_BGN_AT_HRS', '3 15')
+  fcst_len_hrs_cycls=os.getenv('FCST_LEN_HRS_CYCLES', '03 03')
   fcst_length=os.getenv('FCST_LENGTH','1')
   lbc_interval=os.getenv('LBC_INTERVAL','3')
   history_interval=os.getenv('HISTORY_INTERVAL', '1')
@@ -52,6 +22,36 @@ def fcst(xmlFile, expdir, do_ensemble=False):
     'PHYSICS_SUITE': f'{physics_suite}',
     'FCST_LEN_HRS_CYCLES': f'{fcst_len_hrs_cycls}'
   }
+
+  if not do_ensemble:
+    metatask=False
+    task_id=f'{meta_id}'
+    meta_bgn=""
+    meta_end=""
+    RUN='rrfs'
+    ensindexstr=""
+    ensdirstr=""
+    ensstr=""
+  else:
+    metatask=True
+    task_id=f'{meta_id}_m#ens_index#'
+    dcTaskEnv['ENS_INDEX']="#ens_index#"
+    meta_bgn=""
+    meta_end=""
+    ens_size=int(os.getenv('ENS_SIZE','2'))
+    ens_indices=''.join(f'{i:03d} ' for i in range(1,int(ens_size)+1)).strip()
+    meta_bgn=f'''
+<metatask name="ens_{meta_id}">
+<var name="ens_index">{ens_indices}</var>'''
+    meta_end=f'\
+</metatask>\n'
+    RUN='ens'
+    ensindexstr="_m#ens_index#"
+    ensdirstr="/m#ens_index#"
+    ensstr="ens_"
+
+  dcTaskEnv['DATAROOT']=f'<cyclestr>&DATAROOT;/&NET;/&rrfs_ver;/&RUN;.@Y@m@d/@H{ensdirstr}</cyclestr>'
+  dcTaskEnv['MEMDIR']=f'{ensdirstr}'
 
   # dependencies
   hrs=hrs.split(' ')
