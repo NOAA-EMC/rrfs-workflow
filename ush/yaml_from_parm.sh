5343ca60650df6be55c76da3cc5fda643f96f8ce
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-03-07 01:51:52 +0000

    add radiosonde DA into ush/yaml_from_parm.sh

--- ush/yaml_from_parm.sh
+++ ush/yaml_from_parm.sh
@@ -9,3 +9,8 @@ if [[ "${HYB_WGT_ENS}" == "0" ]] || [[ "${HYB_WGT_ENS}" == "0.0" ]]; then # pure
 elif [[ "${HYB_WGT_STATIC}" == "0" ]] || [[ "${HYB_WGT_STATIC}" == "0.0" ]] ; then # pure 3DEnVar
   sed -i '46,87d' ./jedivar.yaml
 fi
+${USHrrfs}/yaml_remove_obs q133,uv233,t120,q120,ps120,uv220
+# comment out the above line and uncomment the follow 3 lines to assimilate radiosonde observations
+#if [[ ! -s "ioda_adpupa.nc" ]]; then
+#  ${USHrrfs}/yaml_remove_obs t120,q120,ps120,uv220
+#fi
