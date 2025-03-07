3d8186a92b7112a8e5b4b76ca4030333be0f2004
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-03-07 16:19:57 -0700

    add an non-NCO option DO_CLEAN to allow clean up com/ and stmp/ directorys

--- workflow/rocoto_funcs/clean.py
+++ workflow/rocoto_funcs/clean.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 # this file hosts all tasks that will not be needed by NCO
 import os
-from rocoto_funcs.base import xml_task, source, get_cascade_env
+from rocoto_funcs.base import xml_task, get_cascade_env
 
 ### begin of clean --------------------------------------------------------
 def clean(xmlFile, expdir):
   task_id='clean'
   cycledefs='prod'
+  #
+  dcTaskEnv={
+    'STMP_RETENTION_HRS': os.getenv("STMP_RETENTION_HRS",'24'),
+    'COM_RETENTION_HRS': os.getenv("COM_RETENTION_HRS",'120'),
+    'CLEAN_BACK_DAYS': os.getenv("CLEAN_BACK_DAYS",'5'),
+  }
+  #
   xml_task(xmlFile,expdir,task_id,cycledefs)
 ### end of clean --------------------------------------------------------
