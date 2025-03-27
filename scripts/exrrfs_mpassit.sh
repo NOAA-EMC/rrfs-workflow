81ac11fab050204b1267c88e79310d27c3379b32
chunhuazhou <chunhua.zhou@noaa.gov>
2025-03-27 22:38:16 +0000

    changes recommended by shellcheck linter

--- scripts/exrrfs_mpassit.sh
+++ scripts/exrrfs_mpassit.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
-declare -rx PS4='+ $(basename ${BASH_SOURCE[0]:-${FUNCNAME[0]:-"Unknown"}})[${LINENO}]${id}: '
+# shellcheck disable=SC2154,SC2153,SC1091
+declare -rx PS4='+ $(basename ${BASH_SOURCE[0]:-${FUNCNAME[0]:-"Unknown"}})[${LINENO}]: '
 set -x
 
 cpreq=${cpreq:-cpreq}
-cd ${DATA}
+cd "${DATA}"  || exit 1
 #
 #  copy excutable and fix files for this mesh
 #
-${cpreq} ${EXECrrfs}/mpassit.x .
+${cpreq} "${EXECrrfs}"/mpassit.x .
 
-${cpreq} ${FIXrrfs}/mpassit/diaglist                    diaglist
-${cpreq} ${FIXrrfs}/mpassit/histlist_2d                 histlist_2d
-${cpreq} ${FIXrrfs}/mpassit/histlist_3d                 histlist_3d
-${cpreq} ${FIXrrfs}/mpassit/histlist_soil               histlist_soil
-${cpreq} ${FIXrrfs}/mpassit/geo_em.d01.nc_${MESH_NAME}  geo_em.d01.nc
+${cpreq} "${FIXrrfs}"/mpassit/diaglist                    diaglist
+${cpreq} "${FIXrrfs}"/mpassit/histlist_2d                 histlist_2d
+${cpreq} "${FIXrrfs}"/mpassit/histlist_3d                 histlist_3d
+${cpreq} "${FIXrrfs}"/mpassit/histlist_soil               histlist_soil
+${cpreq} "${FIXrrfs}/mpassit/geo_em.d01.nc_${MESH_NAME}"  geo_em.d01.nc
 
 #  decide mesh dimensions
 if [[ "${MESH_NAME}" == "conus12km" ]]; then
@@ -28,22 +29,16 @@ elif [[ "${MESH_NAME}" == "conus3km" ]]; then
   ref_lat=38.5
 fi
 #
-#  find cycle time
-#
-YYYYMMDDHH=${CDATE}
-YYYYMMDD=${CDATE:0:8}
-HH=${CDATE:8:2}
-#
 # find init.nc
 #
 n=0
 while [[ $n -le 12 ]]; do
-  CDATEic=$($NDATE -$((10#${n})) ${CDATE})
+  CDATEic=$($NDATE -$((10#${n})) "${CDATE}")
   PDYic=${CDATEic:0:8}
   HHic=${CDATEic:8:2}
   checkfile=${COMINrrfs}/${RUN}.${PDYic}/${HHic}/ic/${WGF}${MEMDIR}/init.nc
   if [[ -s "${checkfile}" ]]; then
-     ln -sf ${checkfile} .
+     ln -sf "${checkfile}" .
      break
   else
      n=$((n + 1))
@@ -54,25 +49,25 @@ done
 #
 fcst_length=${FCST_LENGTH:-1}
 fcst_len_hrs_cycles=${FCST_LEN_HRS_CYCLES:-"01 01"}
-fcst_len_hrs_thiscyc=$(${USHrrfs}/find_fcst_length.sh "${fcst_len_hrs_cycles}" "${cyc}" "${fcst_length}")
+fcst_len_hrs_thiscyc=$("${USHrrfs}/find_fcst_length.sh" "${fcst_len_hrs_cycles}" "${cyc}" "${fcst_length}")
 echo "forecast length for this cycle is ${fcst_len_hrs_thiscyc}"
 #
 # loop through forecast history files for this group
 #
 fhr_string=$( seq 0 $((10#${HISTORY_INTERVAL})) $((10#${fcst_len_hrs_thiscyc} )) )
-fhr_all=(${fhr_string})
+fhr_all=("${fhr_string}")
 num_fhrs=${#fhr_all[@]}
 group_total_num=$((10#${GROUP_TOTAL_NUM}))
 group_index=$((10#${GROUP_INDEX}))
 
-for (( ii=0; ii<${num_fhrs}; ii=ii+${group_total_num} )); do
-    i=$(( ii + ${group_index} - 1 ))
-    if (( $i >= ${num_fhrs} )); then
+for (( ii=0; ii<"${num_fhrs}"; ii=ii+"${group_total_num}" )); do
+    i=$(( ii + "${group_index}" - 1 ))
+    if (( i >= num_fhrs )); then
       break
     fi
     # get forecast hour and string
     fhr=${fhr_all[$i]}
-    CDATEp=$($NDATE ${fhr} ${CDATE} )
+    CDATEp=$(${NDATE} "${fhr}" "${CDATE}" )
     timestr=$(date -d "${CDATEp:0:8} ${CDATEp:8:2}" +%Y-%m-%d_%H.%M.%S) 
     # decide the history files   
     history_file=${UMBRELLA_SAVE_FCST_DATA}/history.${timestr}.nc
@@ -85,21 +80,21 @@ for (( ii=0; ii<${num_fhrs}; ii=ii+${group_total_num} )); do
       sleep 60s
     done
     # run mpassit
-    if [[ -s ${history_file} ]] && [[ -s ${diag_file} ]]; then
-      ln -sfn ${history_file} .
-      ln -sfn ${diag_file} .
+    if [[ -s "${history_file}" ]] && [[ -s "${diag_file}" ]]; then
+      ln -sfn "${history_file}" .
+      ln -sfn "${diag_file}" .
 
       # generate the naemlist on fly
       sed -e "s/@timestr@/${timestr}/" -e "s/@nx@/${nx}/" -e "s/@ny@/${ny}/" -e "s/@dx@/${dx}/" \
-          -e "s/@ref_lat@/${ref_lat}/" ${PARMrrfs}/namelist.mpassit > namelist.mpassit
+          -e "s/@ref_lat@/${ref_lat}/" "${PARMrrfs}/namelist.mpassit" > namelist.mpassit
 
       # run the executable
       source prep_step
       ${MPI_RUN_CMD} ./mpassit.x namelist.mpassit
       # check the status, copy output to UMBRELLA_MPASSIT_DATA
       if [[ -s "./mpassit.${timestr}.nc" ]]; then
-        mv ./mpassit.${timestr}.nc ${UMBRELLA_MPASSIT_DATA}/.
-        mv namelist.mpassit namelist.mpassit_${fhr}
+        mv "./mpassit.${timestr}.nc" "${UMBRELLA_MPASSIT_DATA}/."
+        mv namelist.mpassit "namelist.mpassit_${fhr}"
       else
         echo "FATAL ERROR: failed to genereate mpassit.${timestr}.nc"
         err_exit
