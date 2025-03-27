f14b79efd9640355dfa16c40dedc09d400daa099
lgannoaa <37596169+lgannoaa@users.noreply.github.com>
2025-03-27 14:29:57 +0000

    Brings back the ecflow workflow used by RRFS official realtime parallel to the main branch. (#705)
    Remove developer wire files in ecflow workflow (#653)
    - Performance tuning.
    - Realtime trigger.
    - Removed 13000+ of developer wired environment variable files.
    - Handle storage space clean up.
    - Handle file level scan and trigger with File Service manager (FSM) job.

--- ecf/include/ensf_prdgen_mem001_f033_dev_wire_18.h
+++
@@ -1,1 +0,0 @@
-export GLOBAL_VAR_DEFNS_FP="/lfs/h2/emc/lam/noscrub/emc.lam/rrfs/para/packages/rrfs.v1.0.0/parm/config/ensf/var_defns.sh" export CYCLE_DIR="${DATAROOT}/${RUN}/${WGF}/${PDY}${cyc}" export SLASH_ENSMEM_SUBDIR="/mem0001" export ENSMEM_INDX="0001" export fhr="033" export TMMARK="tm00"
