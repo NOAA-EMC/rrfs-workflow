ac5fa900e919ffaeed33b1c4bcba156901cfca4f
Guoqing Ge <guoqing.ge@noaa.gov>
2025-03-27 06:24:09 -0600

    add a linter for Shell scripts (#698)
    * add a linter for Shell scripts
    * address the comments made by ShellCheck
    * remove the push trigger from linters.yaml as we don't want to run redundant linters at the same time with the PR trigger
    ---------

---
+++ workflow/ush/linter_shellcheck.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+run_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "${run_dir}" || exit
+# shellcheck disable=SC1091
+source detect_machine.sh 
+
+case ${MACHINE} in
+  wcoss2)
+    EXEC_DIR=/to/be/added
+    ;;
+  hera)
+    EXEC_DIR=/scratch1/BMC/wrfruc/gge/Miniforge3/envs/bokeh/bin
+    ;;
+  jet)
+    EXEC_DIR=/lfs6/BMC/wrfruc/gge/Miniforge3/envs/bokeh/bin
+    ;;
+  orion)
+    EXEC_DIR=/work/noaa/zrtrr/gge/Miniforge3/envs/bokeh/bin
+    ;;
+  hercules)
+    EXEC_DIR=/work/noaa/zrtrr/gge/hercules/Miniforge3/envs/bokeh/bin
+    ;;
+  gaea)
+    if [[ -d /gpfs/f5 ]]; then
+      EXEC_DIR=/to/be/added
+    elif [[ -d /gpfs/f6 ]]; then
+      EXEC_DIR=/gpfs/f6/bil-fire10-oar/world-shared/gge/Miniforge3/envs/bokeh/bin
+    else
+      echo "unsupported gaea cluster: ${MACHINE}"
+      exit 1
+    fi
+    ;;
+  *)
+    EXEC_DIR=/unknown/location
+    echo "platform not supported: ${MACHINE}"
+    exit 1
+    ;;
+esac
+
+# ${EXEC_DIR}/shellcheck --color=always $1
+${EXEC_DIR}/shellcheck "$@"
