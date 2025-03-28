dc469ca2da8fb1b9d8ad8052e1abf47ab68f037f
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-03-27 20:27:06 -0600

    done - exrrfs_jedivar.sh

--- scripts/exrrfs_jedivar.sh
+++ scripts/exrrfs_jedivar.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-declare -rx PS4='+ $(basename ${BASH_SOURCE[0]:-${FUNCNAME[0]:-"Unknown"}})[${LINENO}]${id}: '
+# shellcheck disable=SC2154,SC1091,SC2153,SC2034
+declare -rx PS4='+ $(basename ${BASH_SOURCE[0]:-${FUNCNAME[0]:-"Unknown"}})[${LINENO}]: '
 set -x
 
 cpreq=${cpreq:-cpreq}
 prefix=${EXTRN_MDL_SOURCE%_NCO} # remove the trailing '_NCO' if any
-cd ${DATA}
+cd "${DATA}" || exit 1
 
 start_time=$(date -d "${CDATE:0:8} ${CDATE:8:2}" +%Y-%m-%d_%H:%M:%S) 
 timestr=$(date -d "${CDATE:0:8} ${CDATE:8:2}" +%Y-%m-%d_%H.%M.%S) 
@@ -21,33 +22,33 @@ else
   initial_file=${UMBRELLA_PREP_IC_DATA}/mpasin.nc
 fi
 #
-ln -snf ${FIXrrfs}/physics/${PHYSICS_SUITE}/* .
-ln -snf ${FIXrrfs}/meshes/${MESH_NAME}.ugwp_oro_data.nc ./ugwp_oro_data.nc
+ln -snf "${FIXrrfs}/physics/${PHYSICS_SUITE}"/*  .
+ln -snf "${FIXrrfs}/meshes/${MESH_NAME}.ugwp_oro_data.nc"  ./ugwp_oro_data.nc
 zeta_levels=${EXPDIR}/config/ZETA_LEVELS.txt
-nlevel=$(wc -l < ${zeta_levels})
-ln -snf ${FIXrrfs}/meshes/${MESH_NAME}.invariant.nc_L${nlevel}_${prefix} ./invariant.nc
+nlevel=$(wc -l < "${zeta_levels}")
+ln -snf "${FIXrrfs}/meshes/${MESH_NAME}.invariant.nc_L${nlevel}_${prefix}"  ./invariant.nc
 mkdir -p graphinfo stream_list
-ln -snf ${FIXrrfs}/graphinfo/* graphinfo/
-ln -snf ${FIXrrfs}/stream_list/${PHYSICS_SUITE}/* stream_list/
-${cpreq} ${FIXrrfs}/jedi/obsop_name_map.yaml .                  
-${cpreq} ${FIXrrfs}/jedi/keptvars.yaml .              
-${cpreq} ${FIXrrfs}/jedi/geovars.yaml . 
+ln -snf "${FIXrrfs}"/graphinfo/*  graphinfo/
+ln -snf "${FIXrrfs}/stream_list/${PHYSICS_SUITE}"/*  stream_list/
+${cpreq} "${FIXrrfs}"/jedi/obsop_name_map.yaml .
+${cpreq} "${FIXrrfs}"/jedi/keptvars.yaml .
+${cpreq} "${FIXrrfs}"/jedi/geovars.yaml .
 #
-# create data directory 
+# create data directory
 #
-mkdir -p data; cd data                   
+mkdir -p data; cd data || exit 1
 mkdir -p obs ens static_bec
 #
 #  bump files and static BEC files
 #
-ln -snf ${FIXrrfs}/bumploc/${MESH_NAME}_L${nlevel}_${NTASKS}_401km11levels bumploc
-ln -snf ${FIXrrfs}/static_bec/${MESH_NAME}_L${nlevel}/stddev.nc static_bec/stddev.nc
-ln -snf ${FIXrrfs}/static_bec/${MESH_NAME}_L${nlevel}/nicas_${NTASKS} static_bec/nicas
-ln -snf ${FIXrrfs}/static_bec/${MESH_NAME}_L${nlevel}/vbal_${NTASKS} static_bec/vbal
+ln -snf "${FIXrrfs}/bumploc/${MESH_NAME}_L${nlevel}_${NTASKS}_401km11levels"  bumploc
+ln -snf "${FIXrrfs}/static_bec/${MESH_NAME}_L${nlevel}/stddev.nc"  static_bec/stddev.nc
+ln -snf "${FIXrrfs}/static_bec/${MESH_NAME}_L${nlevel}/nicas_${NTASKS}"  static_bec/nicas
+ln -snf "${FIXrrfs}/static_bec/${MESH_NAME}_L${nlevel}/vbal_${NTASKS}"  static_bec/vbal
 #
 # copy observations files
 #
-cp ${COMOUT}/ioda_bufr/det/* obs/.
+cp "${COMOUT}"/ioda_bufr/det/* obs/.
 #
 #  find ensemble forecasts based on user settings
 #
@@ -56,13 +57,13 @@ if [[ "${HYB_WGT_ENS}" != "0" ]] && [[ "${HYB_WGT_ENS}" != "0.0" ]]; then # usin
     echo "use rrfs ensembles"
     mpasout_file=mpasout.${timestr}.nc
     for (( ii=0; ii<4; ii=ii+1 )); do
-       CDATEp=$($NDATE -${ii} ${CDATE} )
+       CDATEp=$($NDATE "-${ii}" "${CDATE}" )
        ensdir=${COMINrrfs}/rrfs.${CDATEp:0:8}/${CDATEp:8:2}
        ensdir_m001=${ensdir}/fcst/enkf/mem001
-       if [[ -s ${ensdir_m001}/${mpasout_file} ]]; then
+       if [[ -s "${ensdir_m001}/${mpasout_file}" ]]; then
          for (( iii=1; iii<31; iii=iii+1 )); do
-            memid=$(printf %03d ${iii})
-            ln -s ${ensdir}/fcst/enkf/mem${memid}/${mpasout_file} ens/mem${memid}.nc
+            memid=$(printf %03d "${iii}")
+            ln -s "${ensdir}/fcst/enkf/mem${memid}/${mpasout_file}" "ens/mem${memid}.nc"
          done
        fi
     done
@@ -77,8 +78,8 @@ fi
 #
 #  link background
 #
-cd ${DATA}
-ln -snf ${initial_file} mpasin.nc
+cd "${DATA}" || exit 1
+ln -snf "${initial_file}"  mpasin.nc
 #
 # generate namelist, streams, and jedivar.yaml on the fly
 run_duration=1:00:00
@@ -98,23 +99,23 @@ else
   echo "Unknown MESH_NAME, exit!"
   err_exit
 fi
-file_content=$(< ${PARMrrfs}/${physics_suite}/namelist.atmosphere) # read in all content
+file_content=$(< "${PARMrrfs}/${physics_suite}/namelist.atmosphere") # read in all content
 eval "echo \"${file_content}\"" > namelist.atmosphere
-${cpreq} ${PARMrrfs}/streams.atmosphere.jedivar streams.atmosphere
+${cpreq} "${PARMrrfs}"/streams.atmosphere.jedivar streams.atmosphere
 analysisDate=""${CDATE:0:4}-${CDATE:4:2}-${CDATE:6:2}T${CDATE:8:2}:00:00Z""
-CDATEm2=$($NDATE -2 ${CDATE})
+CDATEm2=$(${NDATE} -2 "${CDATE}")
 beginDate=""${CDATEm2:0:4}-${CDATEm2:4:2}-${CDATEm2:6:2}T${CDATEm2:8:2}:00:00Z""
 #
 # generate jedivar.yaml based on how YAML_GEN_METHOD is set
 case ${YAML_GEN_METHOD:-1} in
   1) # from ${PARMrrfs}
-    source ${USHrrfs}/yaml_from_parm.sh "jedivar"
+    source "${USHrrfs}"/yaml_from_parm.sh "jedivar"
     ;;
   2) # update placeholders in static yaml from gen_jedivar_yaml_nonjcb.sh
-    source ${USHrrfs}/yaml_replace_placeholders.sh
+    source "${USHrrfs}"/yaml_replace_placeholders.sh
     ;;
   3) # JCB
-    source ${USHrrfs}/yaml_jcb.sh
+    source "${USHrrfs}"/yaml_jcb.sh
     ;;
   *)
     echo "unknown YAML_GEN_METHOD:${YAML_GEN_METHOD}"
@@ -127,7 +128,7 @@ export OOPS_TRACE=1
 export OMP_NUM_THREADS=1
 
 source prep_step
-${cpreq} ${EXECrrfs}/mpasjedi_variational.x .
+${cpreq} "${EXECrrfs}"/mpasjedi_variational.x .
 ${MPI_RUN_CMD} ./mpasjedi_variational.x jedivar.yaml log.out
 # check the status
 export err=$?
@@ -136,13 +137,13 @@ err_chk
 # ncks increments to cold_start IC
 if [[ ${start_type} == "cold" ]]; then
   var_list="pressure_p,rho,qv,qc,qr,qi,qs,qg,ni,nr,ng,nc,nifa,nwfa,volg,surface_pressure,theta,u,uReconstructZonal,uReconstructMeridional"
-  ncks -A -H -v ${var_list} ana.nc mpasin.nc
+  ncks -A -H -v "${var_list}" ana.nc mpasin.nc
   export err=$?
   err_chk
   mv ana.nc ..
 fi
 #
 # the input/output file are linked from the umbrella directory, so no need to copy
-cp ${DATA}/jdiag* ${COMOUT}/jedivar/${WGF}
-cp ${DATA}/jedivar*.yaml ${COMOUT}/jedivar/${WGF}
-cp ${DATA}/log.* ${COMOUT}/jedivar/${WGF}
+cp "${DATA}"/jdiag* "${COMOUT}/jedivar/${WGF}"
+cp "${DATA}"/jedivar*.yaml "${COMOUT}/jedivar/${WGF}"
+cp "${DATA}"/log.* "${COMOUT}/jedivar/${WGF}"
