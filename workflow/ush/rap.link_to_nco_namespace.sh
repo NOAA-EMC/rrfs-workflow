c3d8895e18a446bcad1a8d0fa02a3db3fafe5a7a
Guoqing Ge <guoqing.ge@noaa.gov>
2025-03-31 06:22:02 -0600

    Address the remaining bash scripts so that they all pass the ShellCheck Linter (#711)
    * change the left over [ ] to [[ ]]
    * minor changes in JRRFS_IODA_BUFR and JRRFS_UNGRIB
    * update build scripts so that they pass the shellcheck linter
    * update the remain bash scripts so that they pass the shellcheck linter

--- workflow/ush/rap.link_to_nco_namespace.sh
+++ workflow/ush/rap.link_to_nco_namespace.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export TZ="GMT"
 set -x
-src="/public/data/grib/ftp_rap_hyb/7/0/105/0_151987_30"
+#src="/public/data/grib/ftp_rap_hyb/7/0/105/0_151987_30"
 dst="/lfs5/BMC/nrtrr/NCO_data/rap"
 
 # the workflow provides the $GRBFILE env variables
@@ -13,6 +13,6 @@ HH=${fname:5:2}
 fhr=$(( 10#${fname:7:6} ))
 fhr=$(printf "%02d" ${fhr})
 fpath=${dst}/rap.${PDY}
-mkdir -p ${fpath}
-ln -snf ${GRBFILE} ${fpath}/rap.t${HH}z.wrfnatf${fhr}.grib2
-#ln ${GRBFILE} ${fpath}/rap.t${HH}z.wrfnatf${fhr}.grib2 #do hard links by the file owner if possible
+mkdir -p "${fpath}"
+ln -snf "${GRBFILE}" "${fpath}/rap.t${HH}z.wrfnatf${fhr}.grib2"
+#ln "${GRBFILE}" "${fpath}/rap.t${HH}z.wrfnatf${fhr}.grib2" #do hard links by the file owner if possible
