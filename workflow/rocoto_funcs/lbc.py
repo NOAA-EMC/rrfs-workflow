bd128d9d709e25253686e1832ab23f1445251054
Michael Barlage <michael.barlage@noaa.gov>
2025-04-10 16:07:56 +0000

    remove nsoil_levels from base.py and move to ic/lbc tasks

--- workflow/rocoto_funcs/lbc.py
+++ workflow/rocoto_funcs/lbc.py
@@ -25,6 +25,7 @@ def lbc(xmlFile, expdir, do_ensemble=False):
         'INTERVAL': f'{interval}',
         'GROUP_INDEX': f'#group_index#',
         'GROUP_TOTAL_NUM': f'{lbc_group_total_num}'
+        'NSOIL_LEVELS': os.getenv('NSOIL_LEVELS', '9'),
     }
 
     if not do_ensemble:
