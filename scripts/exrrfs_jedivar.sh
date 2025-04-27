a760c18047a56f973a3893c82dd631b0a25b1ffe
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-04-27 08:54:45 -0600

    allow specifying a HYB_ENS_PATH to use external ensembles

--- scripts/exrrfs_jedivar.sh
+++ scripts/exrrfs_jedivar.sh
@@ -72,7 +72,11 @@ if [[ "${HYB_WGT_ENS}" != "0" ]] && [[ "${HYB_WGT_ENS}" != "0.0" ]]; then # usin
     init_file=init.nc
     for (( ii=0; ii<7; ii=ii+1 )); do
        CDATEp=$(${NDATE} "-${ii}" "${CDATE}" )
-       ensdir=${COMINrrfs}/rrfs.${CDATEp:0:8}/${CDATEp:8:2}
+       if [[ "${HYB_ENS_PATH}" == "" ]]; then
+         ensdir=${COMINrrfs}/rrfs.${CDATEp:0:8}/${CDATEp:8:2}
+       else
+         ensdir=${HYB_ENS_PATH}/rrfs.${CDATEp:0:8}/${CDATEp:8:2}
+       fi
        ensdir_m001=${ensdir}/ic/enkf/mem001
        if [[ -s "${ensdir_m001}/${init_file}" ]]; then
          for (( iii=1; iii<31; iii=iii+1 )); do
