4f272a6017a917d5dac95b5cc83be0dde6d9cf05
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-05-10 20:40:30 -0600

    call ush/find_ensembles.sh in exrrfs_jedivar.sh

--- scripts/exrrfs_jedivar.sh
+++ scripts/exrrfs_jedivar.sh
@@ -57,53 +57,11 @@ cp "${UMBRELLA_PREP_IC_DATA}"/*satbias* satbias_in/.
 #
 # copy observations files
 #
-source "${USHrrsf}/copy_obs.sh"
+source "${USHrrfs}/copy_obs.sh"
 #
 #  find ensemble forecasts based on user settings
 #
-if [[ "${HYB_WGT_ENS}" != "0" ]] && [[ "${HYB_WGT_ENS}" != "0.0" ]]; then # using ensembles
-  if [[ "${HYB_ENS_TYPE}" == "1"  ]]; then # rrfsens
-    echo "use rrfs ensembles"
-    mpasout_file=mpasout.${timestr}.nc
-    for (( ii=0; ii<4; ii=ii+1 )); do
-       CDATEp=$(${NDATE} "-${ii}" "${CDATE}" )
-       if [[ "${HYB_ENS_PATH}" == "" ]]; then
-         ensdir=${COMINrrfs}/rrfs.${CDATEp:0:8}/${CDATEp:8:2}
-       else
-         ensdir=${HYB_ENS_PATH}/rrfs.${CDATEp:0:8}/${CDATEp:8:2}
-       fi
-       ensdir_m001=${ensdir}/fcst/enkf/mem001
-       if [[ -s "${ensdir_m001}/${mpasout_file}" ]]; then
-         for (( iii=1; iii<31; iii=iii+1 )); do
-            memid=$(printf %03d "${iii}")
-            ln -s "${ensdir}/fcst/enkf/mem${memid}/${mpasout_file}" "ens/mem${memid}.nc"
-         done
-       fi
-    done
-  elif [[ "${HYB_ENS_TYPE}" == "2"  ]]; then # interpolated GDAS/GEFFS
-    echo "use interpolated GDAS/GEFS ensembles"
-    init_file=init.nc
-    for (( ii=0; ii<7; ii=ii+1 )); do
-       CDATEp=$(${NDATE} "-${ii}" "${CDATE}" )
-       if [[ "${HYB_ENS_PATH}" == "" ]]; then
-         ensdir=${COMINrrfs}/rrfs.${CDATEp:0:8}/${CDATEp:8:2}
-       else
-         ensdir=${HYB_ENS_PATH}/rrfs.${CDATEp:0:8}/${CDATEp:8:2}
-       fi
-       ensdir_m001=${ensdir}/ic/enkf/mem001
-       if [[ -s "${ensdir_m001}/${init_file}" ]]; then
-         for (( iii=1; iii<31; iii=iii+1 )); do
-            memid=$(printf %03d "${iii}")
-            ln -s "${ensdir}/ic/enkf/mem${memid}/${init_file}" "ens/mem${memid}.nc"
-         done
-       fi
-    done
-
-  elif [[ "${HYB_ENS_TYPE}" == "0"  ]]; then # rrfsens->GDAS->3DVAR
-    echo "determine the ensemble type on the fly"
-    echo "==== to be implemented ===="
-  fi
-fi
+source "${USHrrfs}/find_ensembles.sh"
 #
 #  link background
 #
