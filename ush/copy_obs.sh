475c324be0bf1ba77428c89314593c002512f097
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-05-10 20:55:11 -0600

    make copy_obs.sh work for both JEDIVAR and GETKF

--- ush/copy_obs.sh
+++ ush/copy_obs.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 # copy ioda observation files from com/ to the run directory
 #
-# shellcheck disable=SC2154
-declare -A mappings  # Declare associative array
+# shellcheck disable=SC2154,SC2034
+if [[ "$1" == "jedivar" ]]; then
+  jedivar=true
+else
+  jedivar=false
+fi
 
-obspath="${COMOUT}/ioda_bufr/${WGF}"
+declare -A mappings  # Declare an associative array
+
+if jedivar; then
+  obspath="${COMOUT}/ioda_bufr/${WGF}"
+else # getkf
+  obspath="${COMOUT}/ioda_bufr/${IODA_BUFR_WGF}"
+fi
 mappings["${obspath}/ioda_adpsfc.nc"]="ioda_adpsfc.nc"
 mappings["${obspath}/ioda_adpupa.nc"]="ioda_adpupa.nc"
 mappings["${obspath}/ioda_aircar.nc"]="ioda_aircar.nc"
@@ -16,7 +26,7 @@ mappings["${obspath}/ioda_rassda.nc"]="ioda_rassda.nc"
 mappings["${obspath}/ioda_sfcshp.nc"]="ioda_sfcshp.nc"
 mappings["${obspath}/ioda_vadwnd.nc"]="ioda_vadwnd.nc"
 
-if [ "${DO_ENVAR_RADAR_REF}" == "true" ];then
+if [[ "${DO_ENVAR_RADAR_REF}" == "true" ]] && jedivar; then
   obspath="${COMOUT}/ioda_mrms_refl/${WGF}"
   mappings["${obspath}/ioda_mrms_${CDATE}_${time_min}.nc4"]="ioda_mrms_refl.nc"
 fi
@@ -24,7 +34,7 @@ fi
 # loop through and copy files
 for src_file in "${!mappings[@]}"; do
   if [[ ! -s "${src_file}" ]]; then
-    echo cp "${src_file}"  "${mappings[${src_file}]}"
+    cp "${src_file}"  "${mappings[${src_file}]}"
   else
     echo "WARNING: ${src_file} does not exist!"
   fi
