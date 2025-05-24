9cc219fb0f82ebe4497c6fc6b2e2c0a5cddac934
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-05-24 13:13:02 -0600

    address the linter comment

--- workflow/rocoto_funcs/prep_ic.py
+++ workflow/rocoto_funcs/prep_ic.py
@@ -101,7 +101,7 @@ def prep_ic(xmlFile, expdir, do_ensemble=False, spinup_mode=0):
         starttime = get_cascade_env(f"STARTTIME_{task_id}".upper())
         timedep = f'\n   <timedep><cyclestr offset="{starttime}">@Y@m@d@H@M00</cyclestr></timedep>'
     if os.getenv('DO_IC_LBC', 'TRUE').upper() == "TRUE":
-        icdep="\n      <taskdep task="ic{ensindexstr}"/>{satbias_dep}"
+        icdep='\n      <taskdep task="ic{ensindexstr}"/>{satbias_dep}'
     else:
         icdep=""
     #
