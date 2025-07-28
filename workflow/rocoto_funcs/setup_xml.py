46e70fe5bc4c4c2dde38379ba65484e1f0836c11
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-07-27 18:25:07 -0600

    add derecho in base.py and setup_xml.py

--- workflow/rocoto_funcs/setup_xml.py
+++ workflow/rocoto_funcs/setup_xml.py
@@ -138,6 +138,8 @@ def setup_xml(HOMErrfs, expdir):
         extra = "\nmodule use /ncrc/proj/epic/rocoto/modulefiles"
     elif machine in ['wcoss2']:
         extra = "\nmodule use /apps/ops/test/nco/modulefiles/core"
+    elif machine in ['derecho']:
+        extra = "source /etc/profile.d/z00_modules.sh\nmodule use /glade/work/epicufsrt/contrib/derecho/modulefiles"
     with open(fPath, 'w') as rocotoFile:
         text = \
             f'''#!/usr/bin/env bash
