33e5c127ab7ab7e1143910ede0fbb43aa5364981
chunhua zhou <chunhua.zhou@noaa.gov>
2025-09-10 12:22:21 -0600

    [v2.1.1-9.3] Add ensmean calculation and post-processing (#926)
    * Add ensemble mean calculation and post-processing, optional
    * Modifications based on linter check
    * Update setup_xml.py
    * minor fixes
    * Minor changes per Guoqing's review
    ---------

---
+++ scripts/exrrfs_ensmean.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2153,SC1091,SC2154
+declare -rx PS4='+ $(basename ${BASH_SOURCE[0]:-${FUNCNAME[0]:-"Unknown"}})[${LINENO}]: '
+set -x
+
+cpreq=${cpreq:-cpreq}
+cd "${DATA}"  || exit 1
+#
+# find forecst length for this cycle
+#
+fcst_length=${FCST_LENGTH:-1}
+fcst_len_hrs_cycles=${FCST_LEN_HRS_CYCLES:-"01 01"}
+fcst_len_hrs_thiscyc=$("${USHrrfs}/find_fcst_length.sh" "${fcst_len_hrs_cycles}" "${cyc}" "${fcst_length}")
+echo "forecast length for this cycle is ${fcst_len_hrs_thiscyc}"
+#
+# loop through forecast history files for this group
+#
+fhr_string=$( seq 0 $((10#${HISTORY_INTERVAL})) $((10#${fcst_len_hrs_thiscyc} )) | paste -sd ' ' )
+read -ra fhr_all <<< "${fhr_string}"  # convert fhr_string to an array
+num_fhrs=${#fhr_all[@]}
+group_total_num=$((10#${GROUP_TOTAL_NUM}))
+group_index=$((10#${GROUP_INDEX}))
+
+job_pids=()
+for (( ii=0; ii<"${num_fhrs}"; ii=ii+"${group_total_num}" )); do
+    i=$(( ii + "${group_index}" - 1 ))
+    if (( i >= num_fhrs )); then
+      break
+    fi
+    # get forecast hour and string
+    fhr=${fhr_all[$i]}
+    CDATEp=$(${NDATE} "${fhr}" "${CDATE}" )
+    timestr=$(date -d "${CDATEp:0:8} ${CDATEp:8:2}" +%Y-%m-%d_%H.%M.%S)
+
+    # decide the history files
+    history_mean="${UMBRELLA_ENSMEAN_DATA}/history.${timestr}.nc"
+    diag_mean="${UMBRELLA_ENSMEAN_DATA}/diag.${timestr}.nc"
+    # wait for history file available
+    while true; do
+        historyfiles=("${UMBRELLA_SAVE_FCST_DATA}"/mem*/"history.${timestr}.nc")
+        num_historyfiles=${#historyfiles[@]}
+        if [[ ${num_historyfiles} == "${ENS_SIZE}" ]]; then
+           echo "find enough ensemble history forecast files: ${num_historyfiles} files"
+           break
+        else
+           echo "${num_historyfiles} ensemble history file(s) available, waiting for more ..."
+           sleep 5
+        fi
+    done
+    rm -f  "${history_mean}"
+    echo "Processing ensemble mean for history.${timestr}.nc ..."
+    ncea --no_tmp_fl  "${UMBRELLA_SAVE_FCST_DATA}"/mem*/"history.${timestr}.nc"  "${history_mean}" & job_pids+=($!)
+    # wait for diag file available
+    while true; do
+        diagfiles=("${UMBRELLA_SAVE_FCST_DATA}"/mem*/"diag.${timestr}.nc")
+        num_diagfiles=${#diagfiles[@]}
+        if [[ ${num_diagfiles} == "${ENS_SIZE}" ]]; then
+           echo "find enough ensemble diag forecast files: ${num_diagfiles} files"
+           break
+        else
+           echo "${num_diagfiles} ensemble diag file(s) available, waiting for more ..."
+           sleep 5
+        fi
+    done
+    rm -f "${diag_mean}"
+    echo "Processing ensemble mean for diag.${timestr}.nc ..."
+    ncea --no_tmp_fl  "${UMBRELLA_SAVE_FCST_DATA}"/mem*/"diag.${timestr}.nc"  "${diag_mean}" & job_pids+=($!)
+done
+
+# Wait for all background jobs to finish; exit error if any jobs failed
+for pid in "${job_pids[@]}"; do
+    if ! wait "$pid"; then
+        echo "Ensmean job with PID $pid failed."
+        exit 1
+    fi
+done
+
+echo " All ensemble means computed by forecast hour and saved in ${UMBRELLA_ENSMEAN_DATA}"
+
---
+++ workflow/rocoto_funcs/ensmean.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import os
+from rocoto_funcs.base import xml_task, get_cascade_env
+
+# begin of ensmean --------------------------------------------------------
+
+
+def ensmean(xmlFile, expdir):
+    meta_id = 'ensmean'
+    cycledefs = 'prod'
+    #
+    ensmean_group_total_num = int(os.getenv('ENSMEAN_GROUP_TOTAL_NUM', '3'))
+    fcst_length = os.getenv('FCST_LENGTH', '1')
+    history_interval = os.getenv('HISTORY_INTERVAL', '1')
+    fcst_len_hrs_cycles = os.getenv('FCST_LEN_HRS_CYCLES', '03 03')
+    group_indices = ''.join(f'{i:02d} ' for i in range(
+        1, int(ensmean_group_total_num) + 1, int(history_interval))).strip()
+    fhr2 = ''.join(f'{i:02d} ' for i in range(0, int(ensmean_group_total_num), int(history_interval))).strip()
+    ens_size = int(os.getenv('ENS_SIZE', '2'))
+
+    # Task-specific EnVars beyond the task_common_vars
+    dcTaskEnv = {
+        'FCST_LENGTH': f'{fcst_length}',
+        'HISTORY_INTERVAL': f'{history_interval}',
+        'FCST_LEN_HRS_CYCLES': f'{fcst_len_hrs_cycles}',
+        'GROUP_TOTAL_NUM': f'{ensmean_group_total_num}',
+        'GROUP_INDEX': f'#group_index#',
+        'ENS_SIZE': f'{ens_size}',
+    }
+
+    # metatask (nested or not)
+    fhr = os.getenv('FCST_LENGTH', '3')
+    if int(fhr) >= 100:
+        print(f'FCST_LENGTH>=100 not supported: {fhr}')
+        exit()
+    meta_bgn = f'''
+<metatask name="{meta_id}">
+<var name="group_index">{group_indices}</var>
+<var name="fhr2">{fhr2}</var>'''
+    meta_end = f'</metatask>\n'
+    task_id = f'{meta_id}_g#group_index#'
+
+    # dependencies
+    timedep = ""
+    realtime = os.getenv("REALTIME", "false")
+    if realtime.upper() == "TRUE":
+        starttime = get_cascade_env(f"STARTTIME_{meta_id}".upper())
+        timedep = f'\n    <timedep><cyclestr offset="{starttime}">@Y@m@d@H@M00</cyclestr></timedep>'
+    #
+    dependencies = f'''
+  <dependency>
+  <and>{timedep}
+    <metataskdep metatask="save_fcst"/>
+  </and>
+  </dependency>'''
+    #
+    xml_task(xmlFile, expdir, task_id, cycledefs, dcTaskEnv, dependencies, True, meta_id, meta_bgn, meta_end, "ENSMEAN")
+# end of ensmean --------------------------------------------------------
