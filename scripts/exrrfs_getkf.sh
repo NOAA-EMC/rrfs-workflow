92fe4ee4530b1d6770229c6ec197d03b16f731f6
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-09-17 21:43:20 -0600

    update cold start DA to write out analysis into init.nc directly without ncks

--- scripts/exrrfs_getkf.sh
+++ scripts/exrrfs_getkf.sh
@@ -129,22 +129,7 @@ if [[ ${start_type} == "warm" ]] || [[ ${start_type} == "cold" && ${COLDSTART_CY
     cp "${DATA}"/jdiag* "${COMOUT}/getkf_${GETKF_TYPE}/${WGF}"
     mv jdiag* "${UMBRELLA_GETKF_DATA}"/.
   else # move post mean to umbrella if solver
-    # ncks increments to cold_start IC
-    if [[ "${start_type}" == "cold" ]]; then
-      var_list="pressure_p,rho,qv,qc,qr,qi,qs,qg,ni,nr,ng,nc,nifa,nwfa,volg,surface_pressure,theta,u,uReconstructZonal,uReconstructMeridional,refl10cm,w"
-      for mem in $(seq -w 1 030); do
-        ncks -O -C -x -v ${var_list} "data/ens/mem${mem}.nc" "data/ens/tmp${mem}.nc"
-        ncks -A -v ${var_list} "data/ana/mem${mem}.nc" "data/ens/tmp${mem}.nc"
-        export err=$?
-        err_chk
-        dest=$(readlink -f "data/ens/mem${mem}.nc")
-        mv "data/ens/tmp${mem}.nc" "${dest}"
-      done
-      rm -rf ../ana
-      mv data/ana ../
-    else
-      mv "${DATA}"/data/ens/mem000.nc "${UMBRELLA_GETKF_DATA}"/post_mean.nc
-    fi
+    mv "${DATA}"/data/ens/mem000.nc "${UMBRELLA_GETKF_DATA}"/post_mean.nc
   fi
 
   # Save analysis files if requested
