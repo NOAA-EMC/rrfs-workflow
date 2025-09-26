9dbc573888e5d20df20f80d64fa0934f8b163ae2
Ming.Hu <Ming.Hu@noaa.gov>
2025-09-26 15:18:26 +0000

    Change the JET compiling to use spack-stack 1.6.0 and lfs5 disk. This can compile and configure. But did not test the running.

--- modulefiles/wflow_jet.lua
+++ modulefiles/wflow_jet.lua
@@ -7,16 +7,13 @@ whatis([===[Loads libraries needed for running the RRFS workflow on Jet ]===])
 
 load("rocoto")
 
-prepend_path("MODULEPATH", "/mnt/lfs4/HFIP/hfv3gfs/role.epic/spack-stack/spack-stack-1.5.1/envs/gsi-addon-rocky8//install/modulefiles/Core")
+prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.6.0/envs/gsi-addon-intel/install/modulefiles/Core")
 load(pathJoin("stack-intel", os.getenv("stack_intel_ver") or "2021.5.0"))
 load(pathJoin("stack-intel-oneapi-mpi", os.getenv("stack_impi_ver") or "2021.5.1"))
 load(pathJoin("crtm", os.getenv("crtm_ver") or "2.4.0"))
 
-prepend_path("MODULEPATH","/mnt/lfs4/HFIP/hfv3gfs/role.epic/miniconda3/modulefiles")
-load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.12.0"))
-
 if mode() == "load" then
    LmodMsgRaw([===[Please do the following to activate conda:
-       > conda activate workflow_tools
+       > source /mnt/lfs5/BMC/wrfruc/mhu/miniconda/bin/activate
 ]===])
 end
