ba30e073170f3b2700e29209f2f565bcca72e88e
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-10-01 10:17:48 -0400

    update modulefiles/rrfs/gaea.intel.lua

--- modulefiles/rrfs/gaea.intel.lua
+++ modulefiles/rrfs/gaea.intel.lua
@@ -3,13 +3,13 @@ This module loads libraries for rrfs-workflow
 ]])
 
 whatis([===[Loads libraries for rrfs-workflow ]===])
-prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
 
-load("stack-intel/2023.2.0")
-load("stack-cray-mpich/8.1.29")
-load("cmake/3.23.1")
-load("parallel-netcdf/1.12.2")
-load("parallelio/2.5.10")
+load("stack-oneapi/2024.2.1")
+load("stack-cray-mpich/8.1.32")
+load("cmake/3.27.9")
+load("parallel-netcdf/1.12.3")
+load("parallelio/2.6.2")
 load("libpng/1.6.37")
 
 if mode() == "load" then
