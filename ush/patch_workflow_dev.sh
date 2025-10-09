e0397d62e99f3b69b86448624b2df016b6960ebd
matthew pyle <Matthew.Pyle@noaa.gov>
2025-10-09 12:07:12 +0000

    Deletes unused ush/NCL material
    adds a ush/patch_workflow_dev.sh designed to switch input.nml files to
    use the old 43x64 (52 node) configuration.
    
    running the script creates local changes to these files:
    
    ../parm/config/det/input.nml_18h
    ../parm/config/det/input.nml_long
    ../parm/config/det/input.nml_restart_18h
    ../parm/config/det/input.nml_restart_long
    ../parm/config/det/input.nml_restart_spinupcyc
    ../parm/config/det/input.nml_spinupcyc
    ../parm/config/ensf/input.nml_restart_stoch_ensphy1
    ../parm/config/ensf/input.nml_restart_stoch_ensphy2
    ../parm/config/ensf/input.nml_restart_stoch_ensphy3
    ../parm/config/ensf/input.nml_restart_stoch_ensphy4
    ../parm/config/ensf/input.nml_restart_stoch_ensphy5
    ../scripts/exrrfs_forecast.sh
    
    Still need to get the fix files established on both sides of WCOSS.

---
+++ ush/patch_workflow_dev.sh
@@ -0,0 +1,61 @@
+#! /bin/sh
+
+# Updates namelist files for 52 node configuration
+#
+
+
+files="../parm/config/det/input.nml_18h ../parm/config/det/input.nml_restart_18h"
+
+# 53,128 --> 43,64
+#
+
+for fl in $files
+do
+	cat $fl | sed s:53:43:g > ${fl}_new
+	cat ${fl}_new | sed s:128:64:g > ${fl}
+	rm -f ${fl}_new
+done
+
+
+files="../parm/config/det/input.nml_restart_long ../parm/config/det/input.nml_long"
+
+# 71,128 --> 43,64
+#
+for fl in $files
+do
+	cat $fl | sed s:71:43:g > ${fl}_new
+	cat ${fl}_new | sed s:128:64:g > ${fl}
+	rm -f ${fl}_new
+done
+
+files="../parm/config/det/input.nml_restart_spinupcyc ../parm/config/det/input.nml_spinupcyc"
+# 29,43 --> 43,64
+#
+for fl in $files
+do
+	cat $fl | sed s:29:43:g > ${fl}_new
+	mv ${fl}_new ${fl}
+done
+
+
+files="../parm/config/ensf/input.nml_restart_stoch_ensphy?"
+#
+# 45,128 --> 43,64
+#
+for fl in $files
+do
+	cat $fl | sed s:45:43:g > ${fl}_new
+	cat ${fl}_new | sed s:128:64:g > ${fl}
+	rm -f ${fl}_new
+done
+
+
+# point at dev version of FIX workflow.config file
+#
+
+file="../scripts/exrrfs_forecast.sh"
+
+
+cat ${file} | sed s:workflow.conf:workflow.conf_dev:g > ${file}_new
+mv ${file}_new ${file}
+
