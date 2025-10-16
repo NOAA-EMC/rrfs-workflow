830ebdcca47ed38c2be15d36e5aa496f580dd71c
Ruifang Li <Ruifang.Li@gaea64.ncrc.gov>
2025-10-16 16:20:23 -0600

    Update spack-stack from 1.6.0 to 1.9.2 on Gaea

--- modulefiles/wflow_gaea.lua
+++ modulefiles/wflow_gaea.lua
@@ -5,10 +5,10 @@ the NOAA RDHPC machine Gaea C6
 
 whatis([===[Loads libraries needed for running the UFS SRW App on gaea c6 ]===])
 
-prepend_path("MODULEPATH","/ncrc/proj/epic/spack-stack/c6/spack-stack-1.6.0/envs/gsi-addon/install/modulefiles/Core")
+prepend_path("MODULEPATH","/ncrc/proj/epic/spack-stack/c6/spack-stack-1.9.2/envs/ue-intel-2023.2.0/install/modulefiles/Core")
 load(pathJoin("stack-intel", os.getenv("stack_intel_ver") or "2023.2.0"))
-load(pathJoin("stack-cray-mpich", os.getenv("stack_impi_ver") or "8.1.29"))
-load(pathJoin("crtm", os.getenv("crtm_ver") or "2.4.0"))
+load(pathJoin("stack-cray-mpich", os.getenv("stack_impi_ver") or "8.1.30"))
+load(pathJoin("crtm", os.getenv("crtm_ver") or "2.4.0.1"))
 
 if mode() == "load" then
    LmodMsgRaw([===[Please do the following to activate conda:
