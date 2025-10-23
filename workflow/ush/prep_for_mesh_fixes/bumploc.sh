5974a6b970e7837d305084cbe0dc185ee3d4dc9e
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-10-22 23:27:05 -0600

    add a utility to facilitate generating mesh-dependent fix files (static, invariant, bumploc)

---
+++ workflow/ush/prep_for_mesh_fixes/bumploc.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# shellcheck disable=SC1091
+run_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+HOMErrfs="${run_dir}/../../.."
+
+# replace ./mpasjedi_variational.x with mpasjedi_error_covariance_toolbox.x in exrrfs_jedivar.sh
+sed -i -e 's#${MPI_RUN_CMD} ./mpasjedi_variational.x jedivar.yaml log.out#cp ${HOMErrfs}/workflow/ush/prep_for_mesh_fixes/bumploc.yaml .\n  ${MPI_RUN_CMD} ${HOMErrfs}/sorc/RDASApp/build/bin/mpasjedi_error_covariance_toolbox.x bumploc.yaml log.out#' ${HOMErrfs}/scripts/exrrfs_jedivar.sh
+
+git diff ${HOMErrfs}/scripts/exrrfs_jedivar.sh
+echo -e "\n !! Done !! exrrfs_jedivar.sh has been modified."
+echo "Go to expdir, run 'rboot 202405060000 jedivar' to generate bumploc files"
+echo "Or run 'git checkout ../../../scripts/exrrfs_jedivar.sh' to revert changes"
---
+++ workflow/ush/prep_for_mesh_fixes/invariant.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#
+# shellcheck disable=SC1091
+run_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "${run_dir}/../detect_machine.sh"
+HOMErrfs="${run_dir}/../../.."
+
+case ${MACHINE} in
+  wcoss2)
+    config_geog_data_path=/to/be/added
+    ;;
+  hera|ursa)
+    config_geog_data_path=/scratch3/BMC/wrfruc/mpas/WPS_GEOG/
+    ;;
+  jet)
+    config_geog_data_path=/mnt/lfs5/BMC/wrfruc/HRRRv5/geog/
+    ;;
+  orion|hercules)
+    config_geog_data_path=/work/noaa/zrtrr/WPS_GEOG
+    ;;
+  derecho)
+    config_geog_data_path=/glade/work/geguo/WPS_GEOG
+    ;;
+  gaea)
+    if [[ -d /gpfs/f5 ]]; then
+      config_geog_data_path=/to/be/added
+    elif [[ -d /gpfs/f6 ]]; then
+      config_geog_data_path=/gpfs/f6/arfs-gsl/world-shared/WPS_GEOG/
+    else
+      echo "unsupported gaea cluster: ${MACHINE}"
+    fi
+    ;;
+  *)
+    config_geog_data_path=/to/be/added
+    echo "platform not supported: ${MACHINE}"
+    ;;
+esac
+
+PHYSICS_SUITE=hrrrv5  # modify if using other suites
+
+cp ${run_dir}/streams.init_atmosphere.invariant ${HOMErrfs}/parm/streams.init_atmosphere
+cp ${run_dir}/namelist.init_atmosphere.invariant ${HOMErrfs}/parm/${PHYSICS_SUITE}/namelist.init_atmosphere
+sed -i -e "s#@config_geog_data_path@#${config_geog_data_path}#" ${HOMErrfs}/parm/${PHYSICS_SUITE}/namelist.init_atmosphere 
+#revert the previous change to `scripts/exrrfs_ic.sh`
+git checkout ${HOMErrfs}/scripts/exrrfs_ic.sh
+
+echo -e "\n !! Done !! Changes have been made for generating invariant.nc"
+echo "Go to expdir, run 'rwind 202405060000 ic; rboot 202405060000 ic' to reboot the 'ic' task"
+echo "Or use git checkout to revert the above changes"
