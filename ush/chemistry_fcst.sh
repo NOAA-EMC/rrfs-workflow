f7f880ad3778b50805421934c8e46fba957c55fa
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-11-01 13:06:58 -0600

    update chemistry_fcst.sh and address related linter comments

--- ush/chemistry_fcst.sh
+++ ush/chemistry_fcst.sh
@@ -4,81 +4,90 @@
 # shellcheck disable=SC2154,SC2153
 num_chem=0
 #
-# If any chemistry is activated, cat chemistry information to the namelist and stream_list
+# add chemistry information to the namelist and stream_list
 cat "${PARMrrfs}/chemistry/namelist.atmosphere" >> namelist.atmosphere
 cat "${FIXrrfs}/stream_list/chemistry/stream_list.atmosphere.output" >> ./stream_list/stream_list.atmosphere.output
 #
 # Biogenic/Pollen
 if [[ -r "${UMBRELLA_PREP_CHEM_DATA}/bio.init.nc" ]]; then
-  sed -i '$e cat "${PARMrrfs}"/chemistry/streams.atmosphere.pollen' streams.atmosphere
-  ln -snf ${UMBRELLA_PREP_CHEM_DATA}/bio.init.nc bio.init.nc
+  #sed -i '$e cat "${PARMrrfs}"/chemistry/streams.atmosphere.pollen' streams.atmosphere #gge.debug
+  cat "${PARMrrfs}"/chemistry/streams.atmosphere.pollen >> ./streams.atmosphere
+  ln -snf "${UMBRELLA_PREP_CHEM_DATA}"/bio.init.nc bio.init.nc
   #
-  # Make sure pollen scheme is on
-  if [[ "${DO_POLLEN}" -eq "TRUE" ]]; then
+  if [[ "${CHMISTRY_SPECIES,,}" == *pollen* ]]; then
      sed -i "s/config_pollen_scheme\s*=\s*'off'/config_pollen_scheme  = 'speciated_pollen_primary'/g" namelist.atmosphere
-     num_chem=$(( ${num_chem} + 4 ))
+     num_chem=$(( num_chem + 4 ))
   fi
 fi
 # Dust
 if [[ -r "${UMBRELLA_PREP_CHEM_DATA}/dust.init.nc" ]]; then
-  sed -i '$e cat "${PARMrrfs}"/chemistry/streams.atmosphere.dust' streams.atmosphere
-  ln -snf ${UMBRELLA_PREP_CHEM_DATA}/dust.init.nc dust.init.nc
+  #sed -i '$e cat "${PARMrrfs}"/chemistry/streams.atmosphere.dust' streams.atmosphere
+  cat "${PARMrrfs}"/chemistry/streams.atmosphere.dust >> streams.atmosphere
+  ln -snf "${UMBRELLA_PREP_CHEM_DATA}"/dust.init.nc dust.init.nc
   #
-  # Make sure pollen scheme is on
-  if [[ "${DO_DUST}" -eq "TRUE" ]]; then
+  if [[ "${CHMISTRY_SPECIES,,}" == *dust* ]]; then
      sed -i "s/config_dust_scheme\s*=\s*'off'/config_dust_scheme  = 'on'/g" namelist.atmosphere
-     num_chem=$(( ${num_chem} + 2 ))
+     num_chem=$(( num_chem + 2 ))
   fi
 fi
+#
+# save current nullglob setting and enable nullglob for this script
+save_nullglob=$(shopt -p nullglob)
+shopt -s nullglob
+
 # Anthropogenic
-nanthrofiles=`ls ${UMBRELLA_PREP_CHEM_DATA}/anthro.init* | wc -l`
-if [[ ${nanthrofiles} -gt 0 ]]; then
-  sed -i '$e cat "${PARMrrfs}"/chemistry/streams.atmosphere.anthro' streams.atmosphere
-  ln -snf ${UMBRELLA_PREP_CHEM_DATA}/anthro.init* ./
+files=("${UMBRELLA_PREP_CHEM_DATA}"/anthro.init*)
+if (( ${#files[@]}  )); then  # at least one file exists
+  #sed -i '$e cat "${PARMrrfs}"/chemistry/streams.atmosphere.anthro' streams.atmosphere
+  cat "${PARMrrfs}"/chemistry/streams.atmosphere.anthro >> streams.atmosphere
+  ln -snf "${UMBRELLA_PREP_CHEM_DATA}"/anthro.init* ./
   #
-  # Make sure anthro scheme is on
-  if [[ "${DO_ANTHRO}" -eq "TRUE" ]]; then
+  if [[ "${CHMISTRY_SPECIES,,}" == *anthro* ]]; then
      sed -i "s/config_anthro_scheme\s*=\s*'off'/config_anthro_scheme  = 'on'/g" namelist.atmosphere
-     num_chem=$(( ${num_chem} + 6 ))
-     if [[ "${DO_DUST}" -eq "TRUE" ]]; then
-        num_chem=$(( ${num_chem} - 2 ))
+     num_chem=$(( num_chem + 6 ))
+     if [[ "${CHMISTRY_SPECIES,,}" == *dust* ]]; then
+        num_chem=$(( num_chem - 2 ))
      fi
-     if [[ "${DO_SMOKE}" -eq "TRUE" ]]; then
-        num_chem=$(( ${num_chem} - 2 ))
+     if [[ "${CHMISTRY_SPECIES,,}" == *smoke* ]]; then
+        num_chem=$(( num_chem - 2 ))
      fi
   fi
-  
 fi
 # Smoke/Wildfire
-nfirefiles=`ls ${UMBRELLA_PREP_CHEM_DATA}/smoke.init.* | wc -l`
-if [[ ${nfirefiles} -gt 0 ]]; then
-  #
+files=("${UMBRELLA_PREP_CHEM_DATA}"/smoke.init*)
+if (( ${#files[@]}  )); then  # at least one file exists
   cat "${FIXrrfs}/stream_list/chemistry/stream_list.atmosphere.smoke.output" >> ./stream_list/stream_list.atmosphere.output
   #
-  if [[ "${EBB_DCYCLE}" -eq 1 ]]; then
-     sed -i '$e cat "${PARMrrfs}"/chemistry/streams.atmosphere.smoke_retro' streams.atmosphere
+  if [[ "${EBB_DCYCLE}" -eq 1 ]]; then  # Emissions from Biomass Burning, diurnal cycle? gge.debug
+     #sed -i '$e cat "${PARMrrfs}"/chemistry/streams.atmosphere.smoke_retro' streams.atmosphere
+     cat "${PARMrrfs}"/chemistry/streams.atmosphere.smoke_retro >> streams.atmosphere
   elif [[ "${EBB_DCYCLE}" -eq 2 ]]; then
-     sed -i '$e cat "${PARMrrfs}"/chemistry/streams.atmosphere.smoke_forecast' streams.atmosphere
+     #sed -i '$e cat "${PARMrrfs}"/chemistry/streams.atmosphere.smoke_forecast' streams.atmosphere
+     cat "${PARMrrfs}"/chemistry/streams.atmosphere.smoke_forecast streams.atmosphere
   else
      echo "Not appending any smoke stream"
   fi
   # TODO, retro vs. forecast option
-  ln -snf ${UMBRELLA_PREP_CHEM_DATA}/smoke.init* ./
+  ln -snf "${UMBRELLA_PREP_CHEM_DATA}"/smoke.init* ./
   #
-  # Make sure smoke scheme is on
-  if [[ "${DO_SMOKE}" -eq "TRUE" ]]; then
+  if [[ "${CHMISTRY_SPECIES,,}" == *smoke* ]]; then
      sed -i "s/config_smoke_scheme\s*=\s*'off'/config_smoke_scheme = 'on'/g" namelist.atmosphere
-     num_chem=$(( ${num_chem} + 2 ))
+     num_chem=$(( num_chem + 2 ))
   fi
   # Set EBB_DCYCLE
   sed -e "s/@ebb_dcycle@/${EBB_DCYCLE}/" namelist.atmosphere 
 fi
-# RWC
+# RWC - Residual Woody Combustion? gge.debug
 if [[ -r "${UMBRELLA_PREP_CHEM_DATA}/rwc.init.nc" ]]; then
-  sed -i '$e cat "${PARMrrfs}"/chemistry/streams.atmosphere.rwc' streams.atmosphere
-  ln -snf ${UMBRELLA_PREP_CHEM_DATA}/rwc.init.nc rwc.init.nc
+  #sed -i '$e cat "${PARMrrfs}"/chemistry/streams.atmosphere.rwc' streams.atmosphere
+  cat "${PARMrrfs}"/chemistry/streams.atmosphere.rwc streams.atmosphere
+  ln -snf "${UMBRELLA_PREP_CHEM_DATA}"/rwc.init.nc rwc.init.nc
   # Set namelist
-#      sed -e "s/@online_rwc_emis@/1/" "${PARMrrfs}"/namelist.atmosphere  > namelist.atmosphere 
+  #sed -e "s/@online_rwc_emis@/1/" "${PARMrrfs}"/namelist.atmosphere  > namelist.atmosphere 
 fi
+#
 # Replace the num_chem value with the correct number
 sed -i "s/num_chem\s*=\s*[0-9]*/num_chem  = ${num_chem}/" namelist.atmosphere
+#
+# Restore previous nullglob setting
+eval "${save_nullglob}"
