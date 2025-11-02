bbf753125ba0a0586e89bbc5160de74ca8a68567
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-11-02 10:09:17 -0700

    address linter comments in all prep_[species].sh files

--- ush/prep_dust.sh
+++ ush/prep_dust.sh
@@ -1,4 +1,5 @@
-
+#!/usr/bin/env bash
+# shellcheck disable=SC2154,SC2153
 
 LINKEDEMISFILE=${UMBRELLA_PREP_CHEM_DATA}/dust.init.nc
 
@@ -8,49 +9,50 @@ if [[ "${CREATE_OWN_DATA}" == "TRUE" ]]; then
 else
    DUST_OUTPUTDIR=${DATADIR_CHEM}/dust/processed/
 fi
-${MKDIR} -p ${DUST_OUTPUTDIR}
+mkdir -p "${DUST_OUTPUTDIR}"
 #
 DUST_OUTFILE=${DATADIR_CHEM}/dust/processed/fengsha_dust_inputs.${MESH_NAME}.nc
 #
 if [[ ! -r ${DUST_OUTFILE} ]]; then
-   ${ECHO} "Interpolated dust file: ${DUST_OUTFILE} does not exist, will attempt to create"
-   srun python -u ${SCRIPT}   \
+   echo "Interpolated dust file: ${DUST_OUTFILE} does not exist, will attempt to create"
+   srun python -u "${SCRIPT}" \
               "FENGSHA_1" \
-              ${DATA} \
-              ${DUST_INPUTDIR} \
-              ${DUST_OUTPUTDIR} \
-              ${INTERP_WEIGHTS_DIR} \
-              ${YYYY}${MM}${DD}${HH} \
-              ${MESH_NAME}
+              "${DATA}" \
+              "${DUST_INPUTDIR}" \
+              "${DUST_OUTPUTDIR}" \
+              "${INTERP_WEIGHTS_DIR}" \
+              "${YYYY}${MM}${DD}${HH}" \
+              "${MESH_NAME}"
    OUTFILE_1=${DUST_OUTPUTDIR}/FENGSHA_2022_NESDIS_inputs_${MESH_NAME}_v3.2.nc
 
-   srun python -u ${SCRIPT}   \
+   srun python -u "${SCRIPT}" \
               "FENGSHA_2" \
-              ${DATA} \
-              ${DUST_INPUTDIR} \
-              ${DUST_OUTPUTDIR} \
-              ${INTERP_WEIGHTS_DIR} \
-              ${YYYY}${MM}${DD}${HH} \
-              ${MESH_NAME}
+              "${DATA}" \
+              "${DUST_INPUTDIR}" \
+              "${DUST_OUTPUTDIR}" \
+              "${INTERP_WEIGHTS_DIR}" \
+              "${YYYY}${MM}${DD}${HH}" \
+              "${MESH_NAME}"
    OUTFILE_2=${DUST_OUTPUTDIR}/LAI_GVF_PC_DRAG_CLIMATOLOGY_2024v1.0.${MESH_NAME}.nc
-   ncks -A -v feff ${OUTFILE_2} ${OUTFILE_1}
-   cp ${OUTFILE_1} ${DUST_OUTFILE}
-   ncrename -d Time,nMonths ${DUST_OUTFILE}
-   ncrename -v sep,sep_in -v sandfrac,sandfrac_in -v clayfrac,clayfrac_in -v uthres,uthres_in -v uthres_sg,uthres_sg_in -v feff,feff_m_in ${DUST_OUTFILE}
-   ncdump -hv albedo_drag ${DUST_OUTFILE}
+   ncks -A -v feff "${OUTFILE_2}" "${OUTFILE_1}"
+   cp "${OUTFILE_1}" "${DUST_OUTFILE}"
+   ncrename -d Time,nMonths "${DUST_OUTFILE}"
+   ncrename -v sep,sep_in -v sandfrac,sandfrac_in -v clayfrac,clayfrac_in -v uthres,uthres_in -v uthres_sg,uthres_sg_in -v feff,feff_m_in "${DUST_OUTFILE}"
+   ncdump -hv albedo_drag "${DUST_OUTFILE}"
+   #shellcheck disable=SC2181
    if [[ $? -eq 0 ]]; then
       # Processed old drag
-      ncrename -v albedo_drag,albedo_drag_m_in ${DUST_OUTFILE}
+      ncrename -v albedo_drag,albedo_drag_m_in "${DUST_OUTFILE}"
    else
       # Old drag = new drag
-      ncap2 -O -s 'albedo_drag_m_in=feff_m_in' ${DUST_OUTFILE} ${DUST_OUTFILE}
+      ncap2 -O -s 'albedo_drag_m_in=feff_m_in' "${DUST_OUTFILE}" "${DUST_OUTFILE}"
    fi   
-   ncks -O -6 ${DUST_OUTFILE} ${DUST_OUTFILE}
-   ln -sf ${DUST_OUTFILE} ${LINKEDEMISFILE}
-   timestr3=`date +%Y-%m-%d_%H:00:00 -d "$current_day"`
-   ncap2 -O -s xtime=\"${timestr3}\" ${EMISFILE} ${EMISFILE}  
+   ncks -O -6 "${DUST_OUTFILE}" "${DUST_OUTFILE}"
+   ln -sf "${DUST_OUTFILE}" "${LINKEDEMISFILE}"
+   timestr3=$(date +%Y-%m-%d_%H:00:00 -d "$current_day")
+   #shellcheck disable=SC2086
+   ncap2 -O -s xtime=\"${timestr3}\" "${EMISFILE}" "${EMISFILE}"
 else
    echo "Dust file exists, linking"
-   ln -sf ${DUST_OUTFILE} ${LINKEDEMISFILE}
+   ln -sf "${DUST_OUTFILE}" "${LINKEDEMISFILE}"
 fi
-
--- ush/prep_pollen.sh
+++ ush/prep_pollen.sh
@@ -1,4 +1,5 @@
-
+#!/usr/bin/env bash
+# shellcheck disable=SC2154,SC2153
 #
 EMISINPUTDIR=${DATADIR_CHEM}/emissions/pollen/raw/${YYYY}/
 if [[ "${CREATE_OWN_DATA}" == "TRUE" ]]; then
@@ -6,44 +7,44 @@ if [[ "${CREATE_OWN_DATA}" == "TRUE" ]]; then
 else
    EMISOUTPUTDIR=${DATADIR_CHEM}/emissions/pollen/processed/${YYYY}/
 fi
-${MKDIR} -p ${EMISOUTPUTDIR}
+mkdir -p "${EMISOUTPUTDIR}"
 #
 # --- Do we have emissions regridded to our domain?
 # --- If we already have the emissions, link them..
 #
 EMISFILE=${EMISOUTPUTDIR}/pollen_ef_${MESH_NAME}_${YYYY}_${DOY}.nc
 LINKEDEMISFILE=${UMBRELLA_PREP_CHEM_DATA}/bio.init.nc
-if [ ! -r ${EMISFILE} ]; then
-   ${ECHO} "No pollen input file regridded to this specific day and mesh: ${EMISFILE}, will look for a file to interpoloate"
+if [ ! -r "${EMISFILE}" ]; then
+   echo "No pollen input file regridded to this specific day and mesh: ${EMISFILE}, will look for a file to interpoloate"
 else
-   ${LN} -sf ${EMISFILE} ${LINKEDEMISFILE}
-   ${ECHO} "Linked pollen file ${EMISFILE}, exiting"
-   exit 0
+   ln -sf "${EMISFILE}" "${LINKEDEMISFILE}"
+   echo "Linked pollen file ${EMISFILE}, exiting"
+   exit 0  # do we need to exit the whole exrrfs_prep_chem.sh?
 fi
 #
 # -- Look for the base emission file
 #
 EMISFILE_BASE=${EMISINPUTDIR}/pollen_obs_${YYYY}_BELD6_ef_T_${DOY}.nc
-if [[ -r ${EMISFILE_BASE} ]];then
-   ${ECHO} "Found base emission file: ${EMISFILE_BASE}"
+if [[ -r "${EMISFILE_BASE}" ]];then
+   echo "Found base emission file: ${EMISFILE_BASE}"
 else
-   ${ECHO} "Cannot regrid, no base emission file: ${EMISFILE_BASE}"
-   exit 1
+   echo "Cannot regrid, no base emission file: ${EMISFILE_BASE}"
+   exit 1  # do we need to exit the whole exrrfs_prep_chem.sh?
 fi
-srun python -u ${SCRIPT}   \
+srun python -u "${SCRIPT}" \
            "PECM" \
-           ${DATA} \
-           ${EMISINPUTDIR} \
-           ${EMISOUTPUTDIR} \
-           ${INTERP_WEIGHTS_DIR} \
-           ${YYYY}${MM}${DD}${HH} \
-           ${MESH_NAME}
-if [ ! -r ${EMISFILE} ]; then
-   ${ECHO} "Regrid failed, check the logs"
-   exit 1
+           "${DATA}" \
+           "${EMISINPUTDIR}" \
+           "${EMISOUTPUTDIR}" \
+           "${INTERP_WEIGHTS_DIR}" \
+           "${YYYY}${MM}${DD}${HH}" \
+           "${MESH_NAME}"
+if [ ! -r "${EMISFILE}" ]; then
+   echo "Regrid failed, check the logs"
+   exit 1 # do we need to exit the whole exrrfs_prep_chem.sh?
 else
-   ncrename -v GRA_POLL,e_bio_in_polp_grass -v RAG_POLL,e_bio_in_polp_weed -v TREE_POLL,e_bio_in_polp_tree ${EMISFILE}
-   ncks -O -6 ${EMISFILE} ${EMISFILE}
-   ${LN} -sf ${EMISFILE} ${LINKEDEMISFILE}
-   ${ECHO} "Linked pollen file ${EMISFILE}, exiting"
+   ncrename -v GRA_POLL,e_bio_in_polp_grass -v RAG_POLL,e_bio_in_polp_weed -v TREE_POLL,e_bio_in_polp_tree "${EMISFILE}"
+   ncks -O -6 "${EMISFILE}" "${EMISFILE}"
+   ln -sf "${EMISFILE}" "${LINKEDEMISFILE}"
+   echo "Linked pollen file ${EMISFILE}, exiting"
 fi
