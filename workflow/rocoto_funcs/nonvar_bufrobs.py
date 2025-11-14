80a5c8198a1147fc54ecccc2669a4709eda657d7
ShawnMurdzek-NOAA <113145197+ShawnMurdzek-NOAA@users.noreply.github.com>
2025-11-14 07:33:15 -0700

    [v2.1.2-6.5] [rrfs-mpas-jedi] Add infrastructure for nonvar cloud analysis (#1070)
    * Add nonvar cloud analysis executables to build process
    * Add process_metarcld task to deterministic workflow
    * Remove unused variables from scripts/exrrfs_process_metarcld.sh
    * Add nonvar cloud analysis preprocessing tasks
    This includes renaming the process_metarcld task to proc_bufr_nonvar and including all 3 BUFR processing programs in that task.
    * Initial attempt to add the entirety of the cloud analysis to rrfs-workflow
    Can generate the conus3km deterministic workflow without error. Still need to test whether it runs properly
    * Various cloud analysis run script enhancements
    - Turn off cloud analysis for cold starts. Cloud analysis will crash
      during cold starts because the cldfrac field does not exist
    - Fix name of lightning output file name
    - Save log files from cloud analysis to COM directory
    * Add nonvar cloud analysis executables to build process
    * Rebase on authoritative
    * Remove unused variables from scripts/exrrfs_process_metarcld.sh
    * Continue rebase on authoritative
    * Continue rebase on authoritative (3)
    * Various cloud analysis run script enhancements
    - Turn off cloud analysis for cold starts. Cloud analysis will crash
      during cold starts because the cldfrac field does not exist
    - Fix name of lightning output file name
    - Save log files from cloud analysis to COM directory
    
    * Turn off nonvar cloud analysis by default with conus12km
    
    Current version of nonvar cloud analysis crashes when using conus12km. Will hopefully fix this issue in the near future, but for now, we will simply turn the nonvar cloud analysis off.
    
    * Address github bot comments
    * Change nonvar cloud analysis task names
    * Update scripts/exrrfs_nonvar_reflobs.sh
    * Update workflow/setup_rocoto.py
    * Update scripts/exrrfs_nonvar_cldana.sh
    * Update scripts/exrrfs_nonvar_bufrobs.sh
    * Update RRFS_UTILS hash
    * Remove cloud analysis fix files
    
    No longer needed because the subroutines that write BUFR files in the nonvar_bufrobs task have been commented out
    ---------

---
+++ workflow/rocoto_funcs/nonvar_bufrobs.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import os
+from rocoto_funcs.base import xml_task, get_cascade_env
+
+# begin of nonvar_bufrobs --------------------------------------------------------
+
+
+def nonvar_bufrobs(xmlFile, expdir):
+    task_id = 'nonvar_bufrobs'
+    cycledefs = 'prod'
+    num_spinup_cycledef = int(os.getenv('NUM_SPINUP_CYCLEDEF', '0'))
+    if num_spinup_cycledef == 1:
+        cycledefs = 'prod,spinup'
+    elif num_spinup_cycledef == 2:
+        cycledefs = 'prod,spinup,spinup2'
+    elif num_spinup_cycledef == 3:
+        cycledefs = 'prod,spinup,spinup2,spinup3'
+    OBSPATH = os.getenv("OBSPATH", 'OBSPATH_not_defined')
+    # Task-specific EnVars beyond the task_common_vars
+    dcTaskEnv = {
+        'REFERENCE_TIME': '@Y-@m-@dT@H:00:00Z',
+        'OBSPATH': f'{OBSPATH}',
+        'NONVAR_LARC_NPTS': os.getenv('NONVAR_LARC_NPTS', 'NONVAR_LARC_NPTS_not_defined'),
+        'NONVAR_METAR_IMPACT': os.getenv('NONVAR_METAR_IMPACT', 'NONVAR_METAR_IMPACT_not_defined'),
+        'NONVAR_PROJ_NAME': os.getenv('NONVAR_PROJ_NAME', 'NONVAR_PROJ_NAME_not_defined'),
+        'NONVAR_USERDX': os.getenv('NONVAR_USERDX', 'NONVAR_USERDX_not_defined')
+    }
+
+    dcTaskEnv['KEEPDATA'] = get_cascade_env(f"KEEPDATA_{task_id}".upper()).upper()
+    # dependencies
+    larc_path = f'{OBSPATH}/@Y@m@d@H.rap.t@Hz.lgycld.tm00.bufr_d'
+    lght_path = f'{OBSPATH}/@Y@m@d@H.rap.t@Hz.lghtng.tm00.bufr_d'
+    metar_path = f'{OBSPATH}/@Y@m@d@H.rap.t@Hz.prepbufr.tm00'
+
+    timedep = ""
+    realtime = os.getenv("REALTIME", "false")
+    if realtime.upper() == "TRUE":
+        starttime = get_cascade_env(f"STARTTIME_{task_id}".upper())
+        timedep = f'\n    <timedep><cyclestr offset="{starttime}">@Y@m@d@H@M00</cyclestr></timedep>'
+    #
+    dependencies = f'''
+  <dependency>
+  <and>{timedep}
+    <datadep age="00:05:00"><cyclestr>{larc_path}</cyclestr></datadep>
+    <datadep age="00:05:00"><cyclestr>{lght_path}</cyclestr></datadep>
+    <datadep age="00:05:00"><cyclestr>{metar_path}</cyclestr></datadep>
+  </and>
+  </dependency>'''
+    #
+    xml_task(xmlFile, expdir, task_id, cycledefs, dcTaskEnv, dependencies)
+# end of nonvar_bufrobs --------------------------------------------------------
---
+++ workflow/rocoto_funcs/nonvar_cldana.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import os
+from rocoto_funcs.base import xml_task, get_cascade_env
+
+# begin of nonvar_cldana --------------------------------------------------------
+
+
+def nonvar_cldana(xmlFile, expdir, do_spinup=False):
+    if do_spinup:
+        cycledefs = 'spinup'
+        num_spinup_cycledef = os.getenv('NUM_SPINUP_CYCLEDEF', '1')
+        if num_spinup_cycledef == '2':
+            cycledefs = 'spinup,spinup2'
+        elif num_spinup_cycledef == '3':
+            cycledefs = 'spinup,spinup2,spinup3'
+        task_id = 'nonvar_cldana_spinup'
+    else:
+        cycledefs = 'prod'
+        task_id = 'nonvar_cldana'
+    # Task-specific EnVars beyond the task_common_vars
+    extrn_mdl_source = os.getenv('IC_EXTRN_MDL_NAME', 'IC_PREFIX_not_defined')
+    dcTaskEnv = {
+        'EXTRN_MDL_SOURCE': f'{extrn_mdl_source}',
+    }
+    if do_spinup:
+        dcTaskEnv['DO_SPINUP'] = 'TRUE'
+
+    dcTaskEnv['KEEPDATA'] = get_cascade_env(f"KEEPDATA_{task_id}".upper()).upper()
+    # dependencies
+    timedep = ""
+    realtime = os.getenv("REALTIME", "false")
+    if realtime.upper() == "TRUE":
+        starttime = get_cascade_env(f"STARTTIME_{task_id}".upper())
+        timedep = f'\n    <timedep><cyclestr offset="{starttime}">@Y@m@d@H@M00</cyclestr></timedep>'
+    #
+    prep_ic_dep = ""
+    jedidep = ""
+    if os.getenv("DO_JEDI", "FALSE").upper() == "TRUE":
+        if os.getenv("DO_ENSEMBLE", "FALSE").upper() == "TRUE":
+            jedidep = f'\n    <taskdep task="getkf_solver"/>'
+        elif do_spinup:
+            jedidep = f'\n    <taskdep task="jedivar_spinup"/>'
+        else:
+            jedidep = f'\n    <taskdep task="jedivar"/>'
+    else:
+        prep_ic_dep = f'\n    <taskdep task="prep_ic"/>'
+        if do_spinup:
+            prep_ic_dep = f'\n    <taskdep task="prep_ic_spinup"/>'
+    #
+    dependencies = f'''
+  <dependency>
+  <and>{timedep}{prep_ic_dep}{jedidep}
+    <taskdep task="nonvar_bufrobs"/>
+    <taskdep task="nonvar_reflobs"/>
+  </and>
+  </dependency>'''
+    #
+    xml_task(xmlFile, expdir, task_id, cycledefs, dcTaskEnv, dependencies)
+# end of nonvar_cldana --------------------------------------------------------
