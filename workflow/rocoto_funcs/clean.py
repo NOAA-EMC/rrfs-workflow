35bcfeed3c8dda815731d4de8ec324c8dff06903
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-11-14 13:06:25 -0700

    change retention_hrs to retention_cycs to be more informative for retros

--- workflow/rocoto_funcs/clean.py
+++ workflow/rocoto_funcs/clean.py
@@ -11,9 +11,9 @@ def clean(xmlFile, expdir):
     cycledefs = 'prod'
     #
     dcTaskEnv = {
-        'STMP_RETENTION_HRS': os.getenv("STMP_RETENTION_HRS", '24'),
-        'COM_RETENTION_HRS': os.getenv("COM_RETENTION_HRS", '120'),  # 120 hrs = 5 days
-        'LOG_RETENTION_HRS': os.getenv("LOG_RETENTION_HRS", '840'),  # 840 hrs = 35 days
+        'STMP_RETENTION_CYCS': os.getenv("STMP_RETENTION_CYCS", '24'),
+        'COM_RETENTION_CYCS': os.getenv("COM_RETENTION_CYCS", '120'),  # 120 hrs = 5 days
+        'LOG_RETENTION_CYCS': os.getenv("LOG_RETENTION_CYCS", '840'),  # 840 hrs = 35 days
         # go back 'CLEAN_BACK_DAYS' from the first valid clean hour
         'CLEAN_BACK_DAYS': os.getenv("CLEAN_BACK_DAYS", '5'),
     }
