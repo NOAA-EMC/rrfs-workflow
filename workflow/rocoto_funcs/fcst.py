19474fbb2cbb2b48e5d13e6dedd2661a5ce2187d
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-11-24 23:29:03 -0700

    update chem_prep_dust.sh and other fine tunings

--- workflow/rocoto_funcs/fcst.py
+++ workflow/rocoto_funcs/fcst.py
@@ -40,7 +40,7 @@ def fcst(xmlFile, expdir, do_ensemble=False, do_spinup=False):
     if os.getenv('DO_CHEMISTRY', 'FALSE').upper() == "TRUE":
         dcTaskEnv['EBB_DCYCLE'] = os.getenv('EBB_DCYCLE', 0)
         dcTaskEnv['CHEM_GROUPS'] = os.getenv('CHEM_GROUPS', 'smoke')
-        chemdep = '\n<metataskdep metatask="prep_chem"/>'
+        chemdep = '\n    <metataskdep metatask="prep_chem"/>'
     else:
         chemdep = ""
 
