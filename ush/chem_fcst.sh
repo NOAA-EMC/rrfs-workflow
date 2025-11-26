e84c16a724f7cc4b761980bfa7add3a4a42c34c7
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-11-25 20:36:29 -0700

    update chem_fcst.sh and chem_mpassit.sh to match the new fix/chemistry directory

--- ush/chem_fcst.sh
+++ ush/chem_fcst.sh
@@ -7,7 +7,7 @@ num_chem=0
 #
 # add chemistry information to the namelist and stream_list
 cat "${PARMrrfs}/chemistry/namelist.atmosphere" >> namelist.atmosphere
-cat "${FIXrrfs}/stream_list/chemistry/stream_list.atmosphere.output" >> ./stream_list/stream_list.atmosphere.output
+cat "${FIXrrfs}/chemistry/stream_list/stream_list.atmosphere.output" >> ./stream_list/stream_list.atmosphere.output
 #
 # Biogenic/Pollen
 if [[ -r "${UMBRELLA_PREP_CHEM_DATA}/bio.init.nc" ]]; then
@@ -21,7 +21,7 @@ if [[ -r "${UMBRELLA_PREP_CHEM_DATA}/bio.init.nc" ]]; then
 fi
 # Dust
 if [[ "${CHEM_GROUPS,,}" == *dust* ]]; then
-   ln -snf "${FIXrrfs}/chem_input/dust/fengsha_dust_inputs.${MESH_NAME}.nc" dust.init.nc
+   ln -snf "${FIXrrfs}/chemistry/dust/fengsha_dust_inputs.${MESH_NAME}.nc" dust.init.nc
    sed -i "s/config_dust_scheme\s*=\s*'off'/config_dust_scheme  = 'on'/g" namelist.atmosphere
    num_chem=$(( num_chem + 2 ))
 fi
@@ -51,7 +51,7 @@ fi
 # Smoke/Wildfire
 files=("${UMBRELLA_PREP_CHEM_DATA}"/smoke.init*)
 if (( ${#files[@]}  )); then  # at least one file exists
-  cat "${FIXrrfs}/stream_list/chemistry/stream_list.atmosphere.output.smoke" >> ./stream_list/stream_list.atmosphere.output
+  cat "${FIXrrfs}/chemistry/stream_list/stream_list.atmosphere.output.smoke" >> ./stream_list/stream_list.atmosphere.output
   #
   if [[ "${EBB_DCYCLE}" -eq 1 ]]; then  # Diurnal cycle for EBB (Emissions from Biomass Burning)
      sed -i "\$e cat ${PARMrrfs}/chemistry/streams.atmosphere.smoke_retro" streams.atmosphere
