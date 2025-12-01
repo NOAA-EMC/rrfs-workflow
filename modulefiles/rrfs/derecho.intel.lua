ba81959ece76b2482fc15afc9085bf238478bb92
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-12-01 00:20:09 -0700

    update rrfs-workflow to use spack-stack-1.9.3 on derecho

--- modulefiles/rrfs/derecho.intel.lua
+++ modulefiles/rrfs/derecho.intel.lua
@@ -2,27 +2,24 @@ help([[
 This module loads libraries for rrfs-workflow
 ]])
 
---whatis([===[Loads libraries for rrfs-workflow ]===])
-prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/modulefiles")
-prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.8.0/envs/ue-intel-2021.10.0/install/modulefiles/Core")
+whatis([===[Loads libraries for rrfs-workflow ]===])
+prepend_path("MODULEPATH", '/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.3/envs/rebuild-ue-oneapi-2024.2.1/install/modulefiles/Core')
 
-load("stack-intel/2021.10.0")
-load("stack-cray-mpich/8.1.25")
-
+load("stack-oneapi/2024.2.1")
+load("stack-cray-mpich/8.1.29")
 load("cmake/3.27.9")
 load("parallel-netcdf/1.12.3")
 load("parallelio/2.6.2")
 load("jasper/2.0.32")
 load("libpng/1.6.37")
 
-setenv("CMAKE_C_COMPILER", "mpicc")
-setenv("CMAKE_CXX_COMPILER", "mpic++")
-setenv("CMAKE_Fortran_COMPILER", "mpifort")
-
 if mode() == "load" then
-  --setenv("PNETCDF", os.getenv("PARALLEL_NETCDF_ROOT"))
   setenv("PNETCDF", os.getenv("parallel_netcdf_ROOT"))
 end
 if mode() == "unload" then
   unsetenv("PNETCDF")
 end
+
+setenv("CMAKE_C_COMPILER", "mpicc")
+setenv("CMAKE_CXX_COMPILER", "mpic++")
+setenv("CMAKE_Fortran_COMPILER", "mpifort")
