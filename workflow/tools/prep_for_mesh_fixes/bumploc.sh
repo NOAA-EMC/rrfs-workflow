6df35396963f088d152db1c7ae0e7c2d7b7e36ad
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-12-13 23:33:11 -0500

    add workflow/tools/link_extra_meshes.sh; and others

--- workflow/tools/prep_for_mesh_fixes/bumploc.sh
+++ workflow/tools/prep_for_mesh_fixes/bumploc.sh
@@ -5,7 +5,7 @@ run_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 HOMErrfs="${run_dir}/../../.."
 
 # replace ./mpasjedi_variational.x with mpasjedi_error_covariance_toolbox.x in exrrfs_jedivar.sh
-sed -i -e 's#${MPI_RUN_CMD} ./mpasjedi_variational.x jedivar.yaml log.out#cp ${HOMErrfs}/workflow/tools/prep_for_mesh_fixes/bumploc.yaml .\n  ${MPI_RUN_CMD} ${HOMErrfs}/sorc/RDASApp/build/bin/mpasjedi_error_covariance_toolbox.x bumploc.yaml log.out#' ${HOMErrfs}/scripts/exrrfs_jedivar.sh
+sed -i -e 's#${MPI_RUN_CMD} ./mpasjedi_variational.x jedivar.yaml log.out#cp ${HOMErrfs}/workflow/tools/prep_for_mesh_fixes/bumploc.yaml .\n  ${MPI_RUN_CMD} ${HOMErrfs}/sorc/RDASApp/build/bin/mpasjedi_error_covariance_toolbox.x bumploc.yaml log.out\n  err_exit#' ${HOMErrfs}/scripts/exrrfs_jedivar.sh
 
 git diff ${HOMErrfs}/scripts/exrrfs_jedivar.sh
 echo -e "\n !! Done !! exrrfs_jedivar.sh has been modified."
