b58f18479aa5f556f6257404931ceb52e1c07ec3
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-12-30 12:10:01 -0700

    remove no-longer-needed temporary files

--- parm/jcb-rrfs/obs/addj2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for myyaml in *.yaml; do
-  mv ${myyaml} ${myyaml}.j2
-done
--- parm/jcb-rrfs/obs/usej2Template.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for myyaml in *.yaml.j2; do
-  sed -i -e "s/@emptyObsSpaceAction@/{{emptyObsSpaceAction}}/g" -e "s/@analysisDate@/{{analysisDate}}/g" ${myyaml}
-done
