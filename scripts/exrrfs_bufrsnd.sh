bfea9eb1b60099c5659d433b0995099475f5face
Matthew Pyle <matthew.pyle@noaa.gov>
2026-01-28 16:25:05 +0000

    Adds copy of GEMPAK bufrsnd output to regular COMOUT/gempak DBNET alerts these GEMPAK files.

--- scripts/exrrfs_bufrsnd.sh
+++ scripts/exrrfs_bufrsnd.sh
@@ -279,7 +279,12 @@ mv errfile errfile_rrfs_sndp
 if [[ "${SENDCOM}" = "YES" ]]; then
   cpreq $DATA/class1.bufr $COMOUT/rrfs.t${cyc}z.class1.bufr
   cpreq $DATA/profilm.c1.${tmmark} ${COMOUT}/rrfs.t${cyc}z.profilm.c1
-fi
+
+ if [[ "${SENDDBN}" = "YES" ]]; then
+   $DBNROOT/bin/dbn_alert ????  $NET $job ${COMOUT}/rrfs.t${cyc}z.class1.bufr
+ fi
+
+fi #SENDCOM
 
 # remove bufr file breakout directory in $COMOUT if it exists
 
@@ -345,6 +350,16 @@ r
 
 exit
 EOF
+
+if [[ "${SENDCOM}" = "YES" ]]; then
+  cpreq ${outfilbase}.snd ${COMOUT}/gempak/
+  cpreq ${outfilbase}.sfc* ${COMOUT}/gempak/
+ if [[ "${SENDDBN}" = "YES" ]]; then
+  $DBNROOT/bin/dbn_alert ????  $NET $job ${COMOUT}/gempak/${outfilbase}.snd
+  $DBNROOT/bin/dbn_alert ????  $NET $job ${COMOUT}/gempak/${outfilbase}.sfc
+  $DBNROOT/bin/dbn_alert ????  $NET $job ${COMOUT}/gempak/${outfilbase}.sfc_aux
+ fi
+fi
 
 print_info_msg "
 ========================================================================
