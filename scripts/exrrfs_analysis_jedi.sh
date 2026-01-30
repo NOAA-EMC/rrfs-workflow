dda0026f276ffc0e97ac41861b6884b77c7a7408
Samuel Degelia <samuel.degelia@dlogin05.dogwood.wcoss2.ncep.noaa.gov>
2026-01-30 15:15:11 +0000

    remove old commented lines for debugging

--- scripts/exrrfs_analysis_jedi.sh
+++ scripts/exrrfs_analysis_jedi.sh
@@ -180,11 +180,11 @@ if  [ ${ob_type} != "conv" ] || [ ${BKTYPE} -eq 1 ]; then #not using GDAS
   l_both_fv3sar_gfs_ens=.false.
 fi
 
-#if [ -d "${bkpath}.jedi" ]; then #@@@ TODO: UNCOMMENT ME AFTER DEBUGGING
-#  rm -rf "${bkpath}.jedi"  #@@@ TODO: UNCOMMENT ME AFTER DEBUGGING
-#fi  #@@@ TODO: UNCOMMENT ME AFTER DEBUGGING
-#rm -rf ${bkpath}.jedi #@@@ TODO: UNCOMMENT ME AFTER DEBUGGING
-#cp -rL $bkpath ${bkpath}.jedi #@@@ TODO: UNCOMMENT ME AFTER DEBUGGING
+if [ -d "${bkpath}.jedi" ]; then
+  rm -rf "${bkpath}.jedi"
+fi
+rm -rf ${bkpath}.jedi
+cp -rL $bkpath ${bkpath}.jedi
 bkpath=${bkpath}.jedi
 
 #
