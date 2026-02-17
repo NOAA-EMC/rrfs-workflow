89c75ff6bd36ec2a23ee12598586f3cfbf66e0ef
guoqing.ge <Guoqing.Ge@noaa.gov>
2026-02-17 11:48:40 -0700

    hotfix on finding ensembles

--- scripts/exrrfs_jedivar.sh
+++ scripts/exrrfs_jedivar.sh
@@ -87,7 +87,7 @@ source "${USHrrfs}/find_ensembles.sh"
 # check number of ensemble files, if not enough, default to pure 3DVar
 #
 ens_size=$(( 10#${ENS_SIZE} ))
-ens_count=$(find ens -path "ens/mem*.nc" -type f | wc -l)
+ens_count=$(find ens -name "mem*.nc" | wc -l)
 if (( ens_count < ens_size )); then
    echo "Number of ensemble files is ${ens_count}, less than 30, default to 3DVar"
    export HYB_WGT_ENS=0.0
