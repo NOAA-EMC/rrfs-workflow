d7bde1aeb36b613b23c2a64da968894527452b63
chunhuazhou <chunhua.zhou@noaa.gov>
2026-03-03 19:25:26 +0000

    Combine member save_for_next into one task if do_ensemble

--- workflow/rocoto_funcs/save_for_next.py
+++ workflow/rocoto_funcs/save_for_next.py
@@ -7,6 +7,7 @@ from rocoto_funcs.base import xml_task, get_cascade_env
 
 def save_for_next(xmlFile, expdir, do_ensemble=False, do_spinup=False):
     meta_id = 'save_for_next'
+    cyc_interval = os.getenv('CYC_INTERVAL')
     if do_spinup:
         cycledefs = 'spinup'
     else:
@@ -17,36 +18,31 @@ def save_for_next(xmlFile, expdir, do_ensemble=False, do_spinup=False):
         'CYC_INTERVAL': os.getenv('CYC_INTERVAL', '3'),
     }
 
-    if not do_ensemble:
-        metatask = False
-        if do_spinup:
-            task_id = f'{meta_id}_spinup'
-        else:
-            task_id = f'{meta_id}'
-        meta_bgn = ""
-        meta_end = ""
-        ensdirstr = ""
+    metatask = False
+    if do_spinup:
+        task_id = f'{meta_id}_spinup'
     else:
-        metatask = True
-        task_id = f'{meta_id}_m#ens_index#'
-        dcTaskEnv['ENS_INDEX'] = "#ens_index#"
-        meta_bgn = ""
-        meta_end = ""
+        task_id = f'{meta_id}'
+    meta_bgn = ""
+    meta_end = ""
+
+    if do_ensemble:
         ens_size = int(os.getenv('ENS_SIZE', '2'))
-        ens_indices = ''.join(f'{i:03d} ' for i in range(1, int(ens_size) + 1)).strip()
-        meta_bgn = f'''
-<metatask name="{meta_id}">
-<var name="ens_index">{ens_indices}</var>'''
-        meta_end = f'\
-</metatask>\n'
-        ensdirstr = "/mem#ens_index#"
+        dcTaskEnv['ENS_SIZE'] = str(ens_size)
 
     dcTaskEnv['KEEPDATA'] = get_cascade_env(f"KEEPDATA_{task_id}".upper()).upper()
     # dependencies
-    if do_spinup:
-        datadep = f'''<datadep age="00:01:00"><cyclestr>&DATAROOT;/@Y@m@d/&RUN;_fcst_spinup_@H_&rrfs_ver;/&WGF;{ensdirstr}</cyclestr><cyclestr offset="1:00:00">/mpasout.@Y-@m-@d_@H.@M.@S.nc</cyclestr></datadep>'''
+    if do_ensemble:
+        datadep = ""
+        for i in range(1, int(ens_size) + 1):
+            memdirstr = f'/mem{i:03d}'
+            datadep = datadep + f'''\n<datadep age="00:01:00"><cyclestr>&DATAROOT;/@Y@m@d/&RUN;_fcst_@H_&rrfs_ver;/&WGF;{memdirstr}</cyclestr><cyclestr offset="{cyc_interval}:00:00">/mpasout.@Y-@m-@d_@H.@M.@S.nc</cyclestr></datadep>'''
     else:
-        datadep = f'''<datadep age="00:01:00"><cyclestr>&DATAROOT;/@Y@m@d/&RUN;_fcst_@H_&rrfs_ver;/&WGF;{ensdirstr}</cyclestr><cyclestr offset="1:00:00">/mpasout.@Y-@m-@d_@H.@M.@S.nc</cyclestr></datadep>'''
+        if do_spinup:
+            datadep = f'''<datadep age="00:01:00"><cyclestr>&DATAROOT;/@Y@m@d/&RUN;_fcst_spinup_@H_&rrfs_ver;/&WGF;</cyclestr><cyclestr offset="{cyc_interval}:00:00">/mpasout.@Y-@m-@d_@H.@M.@S.nc</cyclestr></datadep>'''
+        else:
+            datadep = f'''<datadep age="00:01:00"><cyclestr>&DATAROOT;/@Y@m@d/&RUN;_fcst_@H_&rrfs_ver;/&WGF;</cyclestr><cyclestr offset="{cyc_interval}:00:00">/mpasout.@Y-@m-@d_@H.@M.@S.nc</cyclestr></datadep>'''
+
     timedep = ""
     realtime = os.getenv("REALTIME", "false")
     if realtime.upper() == "TRUE":
