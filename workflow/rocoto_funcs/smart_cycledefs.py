4ce99c8042e6c895050cf2056bad59cdac087723
guoqing.ge <Guoqing.Ge@noaa.gov>
2026-03-17 11:24:50 -0400

    fine tuning

--- workflow/rocoto_funcs/smart_cycledefs.py
+++ workflow/rocoto_funcs/smart_cycledefs.py
@@ -7,9 +7,6 @@ def smart_cycledefs():
     # If users set CYCLEDEF_* variables explicitly in exp.setup, then just use it
     # otherwise calculate cycledef smartly
 
-    warmda_only = os.getenv('COLDSTART_CYCS_DO_DA', 'FALSE').upper() == "FALSE"
-    cycledef_warmda = ""
-
     cycledef_ic = os.getenv('CYCLEDEF_IC', 'not_defined')
     if cycledef_ic != 'not_defined':
         cycledef_lbc = os.getenv('CYCLEDEF_LBC', 'not_defined')
@@ -40,20 +37,22 @@ def smart_cycledefs():
         # retros write out cycledefs explicitly without referencing XML entities
         else:
             retrodates = os.getenv('RETRO_PERIOD', '2225010100-2225010800').split("-")
-            hour1 = retrodates[0][8:10]
-            if spinup.upper() == "TRUE":
-                if hour1 < 12:  # determine the first prod_cyc
-                    prod_cyc1 = prodswitch_cycs[0]
-                else:
-                    prod_cyc1 = prodswitch_cycs[0]
+            hour1 = int(retrodates[0][8:10])
+            if len(cold_cycs) > 1:
+                index = 0 if hour1 < 12 else 1
             else:
-                prod_cyc1 = cold_cycs[0]  # no spin up, prod_cyc starts from the first cold start cyc
+                index = 0
+            cold_cyc1 = cold_cycs[index]
+            lbc_cyc1 = lbc_cycs[index]
+            prod_cyc1 = cold_cyc1
+            if spinup.upper() == "TRUE":  # if spinup, the first prod_cyc is from prodswitch_cycs
+                prod_cyc1 = prodswitch_cycs[index]
             #
-            cycledef_ic = f'  {retrodates[0][0:8]}{cold_cycs[0]}00 {retrodates[1]}00 {ic_step.zfill(2)}:00:00'
-            cycledef_lbc = f' {retrodates[0][0:8]}{lbc_cycs[0]}00 {retrodates[1]}00 {lbc_step.zfill(2)}:00:00'
+            cycledef_ic = f'  {retrodates[0][0:8]}{cold_cyc1}00 {retrodates[1]}00 {ic_step.zfill(2)}:00:00'
+            cycledef_lbc = f' {retrodates[0][0:8]}{lbc_cyc1}00 {retrodates[1]}00 {lbc_step.zfill(2)}:00:00'
             cycledef_prod = f'{retrodates[0][0:8]}{prod_cyc1}00 {retrodates[1]}00 {cyc_interval.zfill(2)}:00:00'
             if spinup.upper() == "TRUE":
-                cycledef_spinup = f'{retrodates[0][0:8]}{cold_cycs[0]}00 {retrodates[1]}00 {cyc_interval.zfill(2)}:00:00'
+                cycledef_spinup = f'{retrodates[0][0:8]}{cold_cyc1}00 {retrodates[1]}00 {cyc_interval.zfill(2)}:00:00'
     #
     # fill in the Cycledef dictionary
     dcCycledef = {}
