6fcc6a08f5ac7c30d5501f470e8e9342b75e4534
Ruifang Li <Ruifang.Li@gaea65.ncrc.gov>
2026-03-25 17:46:32 -0600

    Update prep_ic script to add blending option.

--- scripts/exrrfs_prep_ic.sh
+++ scripts/exrrfs_prep_ic.sh
@@ -102,55 +102,55 @@ for index in "${mem_list[@]}"; do # loop through all the members
   # do sfc cycling if no global blending
   #
   if [[ "${DO_BLENDING^^}" == "FALSE" ]]; then
-  for hr in ${SFC_UPDATE_CYCS:-"99"}; do
-    shr=$(printf '%02d' $((10#$hr)) )
-    var_list="smois,snow,snowh,snowc,tslb"
-    if [ "${cyc}" == "${shr}" ]; then
-      source_file=""
-      # look back ${NUM} cycles to find mpasout files for surface cycling
-      NUM=3
-      for (( ii=cyc_interval; ii<=$(( NUM*cyc_interval )); ii=ii+cyc_interval )); do
-        CDATEp=$(${NDATE} -${ii} "${CDATE}" )
-        PDYii=${CDATEp:0:8}
-        cycii=${CDATEp:8:2}
-        file_mpasout="${COMINsfc}/${RUN}.${PDYii}/${cycii}/fcst/${WGF}${memdir}/mpasout.${timestr}.nc"
-        if [[ -s "${file_mpasout}" ]]; then
-          source_file="${file_mpasout}"
-          break
-	else
-          echo "SFC_UPDATE: cannot find source file for sfc state: ${file_mpasout}"
-        fi
-      done
-      # if no mpasout files, use init.nc from another run
-      if [[ ! -s "${source_file}" ]]; then
-        if [[  "${COMINsfc}" == "${COMINrrfs}" ]]; then
-          echo "SFC_UPDATE: ${COMINsfc} = ${COMINrrfs}, same init.nc,  skip sfc_update!"
-        else
-          PDYii=${CDATE:0:8}
-          cycii=${CDATE:8:2}
-          file_init="${COMINsfc}/${RUN}.${PDYii}/${cycii}/ic/${WGF}${memdir}/init.nc"
-          if [[ -s "${file_init}" ]]; then
-            source_file="${file_init}"
+    for hr in ${SFC_UPDATE_CYCS:-"99"}; do
+      shr=$(printf '%02d' $((10#$hr)) )
+      var_list="smois,snow,snowh,snowc,tslb"
+      if [ "${cyc}" == "${shr}" ]; then
+        source_file=""
+        # look back ${NUM} cycles to find mpasout files for surface cycling
+        NUM=3
+        for (( ii=cyc_interval; ii<=$(( NUM*cyc_interval )); ii=ii+cyc_interval )); do
+          CDATEp=$(${NDATE} -${ii} "${CDATE}" )
+          PDYii=${CDATEp:0:8}
+          cycii=${CDATEp:8:2}
+          file_mpasout="${COMINsfc}/${RUN}.${PDYii}/${cycii}/fcst/${WGF}${memdir}/mpasout.${timestr}.nc"
+          if [[ -s "${file_mpasout}" ]]; then
+            source_file="${file_mpasout}"
+            break
 	  else
-            echo "SFC_UPDATE: cannot find source file for sfc state: ${file_init}"
+            echo "SFC_UPDATE: cannot find source file for sfc state: ${file_mpasout}"
+          fi
+        done
+        # if no mpasout files, use init.nc from another run
+        if [[ ! -s "${source_file}" ]]; then
+          if [[  "${COMINsfc}" == "${COMINrrfs}" ]]; then
+            echo "SFC_UPDATE: ${COMINsfc} = ${COMINrrfs}, same init.nc,  skip sfc_update!"
+          else
+            PDYii=${CDATE:0:8}
+            cycii=${CDATE:8:2}
+            file_init="${COMINsfc}/${RUN}.${PDYii}/${cycii}/ic/${WGF}${memdir}/init.nc"
+            if [[ -s "${file_init}" ]]; then
+              source_file="${file_init}"
+	    else
+              echo "SFC_UPDATE: cannot find source file for sfc state: ${file_init}"
+            fi
           fi
         fi
-      fi
-      #
-      to_file=""
-      if [[ -s "${source_file}" ]]; then
-	if [[ "${start_type}" == "cold" ]]; then
-          to_file="${umbrella_prep_ic_mem}/init.nc"
-	elif [[ "${start_type}" == "warm" ]]; then
-          to_file="${umbrella_prep_ic_mem}/mpasout.nc"
+        #
+        to_file=""
+        if [[ -s "${source_file}" ]]; then
+	  if [[ "${start_type}" == "cold" ]]; then
+            to_file="${umbrella_prep_ic_mem}/init.nc"
+	  elif [[ "${start_type}" == "warm" ]]; then
+            to_file="${umbrella_prep_ic_mem}/mpasout.nc"
+          fi
+          echo "surface update from ${source_file} to ${to_file}"
+          echo ncks -A -v  "${var_list}"  "${source_file}"  "${to_file}" >>  "$CMDFILE"
+        else
+          echo "SFC_UPDATE failed, cannot find source file for sfc state: ${source_file}"
         fi
-        echo "surface update from ${source_file} to ${to_file}"
-        echo ncks -A -v  "${var_list}"  "${source_file}"  "${to_file}" >>  "$CMDFILE"
-      else
-        echo "SFC_UPDATE failed, cannot find source file for sfc state: ${source_file}"
       fi
-    fi
-  done
+    done
   fi
   #
   #  find the right satbias file
