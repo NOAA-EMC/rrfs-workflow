a318c30de43c8b5aee9af740148f2770a042699e
guoqing.ge <Guoqing.Ge@noaa.gov>
2026-03-29 13:43:59 -0600

    hook up the pyDAmonitor task and other fine tuning

--- workflow/rocoto_funcs/pyDAmonitor.py
+++ workflow/rocoto_funcs/pyDAmonitor.py
@@ -6,10 +6,21 @@ from rocoto_funcs.base import xml_task
 # begin of pyDAmonitor --------------------------------------------------------
 
 
-def pyDAmonitor(xmlFile, expdir):
+def pyDAmonitor(xmlFile, expdir, spinup_mode=0):
     task_id = 'pyDAmonitor'
-    cycledefs = 'prod'
-    #
+    nocoldda = os.getenv('COLDSTART_CYCS_DO_DA', 'TRUE').upper() == 'FALSE'
+    do_spinup = spinup_mode == 1
+    if do_spinup:
+        if nocoldda:
+            cycledefs = 'da_nocold'
+        else:
+            cycledefs = 'spinup'
+    else:
+        if spinup_mode == 0 and nocoldda:
+            cycledefs = 'da_nocold'
+        else:
+            cycledefs = 'prod'
+    # Task-specific EnVars beyond the task_common_vars
     dcTaskEnv = {
         'CHECK_IS_CYC_DONE': os.getenv("CHECK_IS_CYC_DONE", "FALSE"),  # default: TRUE for retros and FALSE for realtime
     }
