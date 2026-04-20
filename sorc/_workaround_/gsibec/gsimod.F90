4af55d47c05876812bc9991837849191d7142a00
Donald.E.Lippi <donald.e.lippi@noaa.gov>
2026-04-20 13:18:02 +0000

    Update the rdasapp hash and some workaround codes

--- sorc/_workaround_/gsibec/gsimod.F90
+++ sorc/_workaround_/gsibec/gsimod.F90
@@ -44,9 +44,12 @@
      init_grid,init_grid_vars,&
      nlayers,jcap,jcap_b,vlevs,&
      use_sp_eqspace,final_grid_vars,&
-     jcap_gfs,nlat_gfs,nlon_gfs,jcap_cut
+     jcap_gfs,nlat_gfs,nlon_gfs,jcap_cut,&
+     rlat_start,rlat_end,&
+     rlon_start,rlon_end,&
+     north_pole_lat,north_pole_lon
 
-  use gridmod, only: init_reg_glob_ll,regional,fv3_regional,grid_ratio_fv3_regional,mpas_regional
+  use gridmod, only: init_reg_glob_ll,regional,fv3_regional,grid_ratio_fv3_regional,mpas_regional,use_fv3_grid_spec
 
   use constants, only: zero,one,init_constants,gps_constants,three
   use constants, only: init_constants,init_constants_derived
@@ -388,7 +391,8 @@
 
 
   namelist/gridopts/jcap,jcap_b,nlat,nlon,nsig,use_sp_eqspace,fv3_regional,grid_ratio_fv3_regional,&
-                    regional,mpas_regional
+                    regional,mpas_regional,use_fv3_grid_spec,rlat_start,rlat_end,rlon_start,rlon_end,&
+                    north_pole_lat,north_pole_lon
 
 ! BKGERR (background error related variables):
 !     vs       - scale factor for vertical correlation lengths for background error
@@ -703,10 +707,6 @@
      switch_on_derivatives = .true.
      if (mype==0) write(6,*)'GSIMOD:  tendencies and derivatives are on'
   endif
-
-!  if (regional) then
-!     call convert_fv3_regional
-!  endif
 
 ! Initialize variables, create/initialize arrays
   lendian_in = -1
