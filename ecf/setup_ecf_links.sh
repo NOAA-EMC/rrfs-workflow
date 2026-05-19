99fa24716e9a8e84509b69a337b7fd5a7b33d196
matthew pyle <Matthew.Pyle@noaa.gov>
2026-05-19 16:58:56 +0000

    Establishes 103nodes for mems 3-5 on my fork.

--- ecf/setup_ecf_links.sh
+++ ecf/setup_ecf_links.sh
@@ -452,9 +452,16 @@ done
 cd $ECF_DIR/scripts/forecast/ensf
 echo "Copy ensf forecast files ..."
 rm -f jrrfs_ensf_forecast_mem???.ecf
-for mem in $(seq 1 5); do
+for mem in $(seq 1 2); do
   mem_3d=$( printf "%03d" "${mem}" )
   cp jrrfs_ensf_forecast_master.ecf jrrfs_ensf_forecast_mem${mem_3d}.ecf
+  sed -i -e "s|@ensf_forecast_member@|${mem_3d}|g" jrrfs_ensf_forecast_mem${mem_3d}.ecf
+  add_to_tmpfile "$ECF_DIR/scripts/forecast/ensf/jrrfs_ensf_forecast_mem${mem_3d}.ecf"
+done
+
+for mem in $(seq 3 5); do
+  mem_3d=$( printf "%03d" "${mem}" )
+  cp jrrfs_ensf_forecast_master.ecf_103nodes jrrfs_ensf_forecast_mem${mem_3d}.ecf
   sed -i -e "s|@ensf_forecast_member@|${mem_3d}|g" jrrfs_ensf_forecast_mem${mem_3d}.ecf
   add_to_tmpfile "$ECF_DIR/scripts/forecast/ensf/jrrfs_ensf_forecast_mem${mem_3d}.ecf"
 done
