abc18479a78f87dbe40519f3513a144e4f89ce1c
chunhua.zhou <Chunhua.Zhou@noaa.gov>
2026-06-11 17:36:44 +0000

    Add WGF to nclprd/

--- workflow/sideload/graphics.sh
+++ workflow/sideload/graphics.sh
@@ -18,7 +18,7 @@ ntasks=${NTASKS:-12}
 grib2_dir="${COMOUT}/upp/det"
 tile=${TILE:-'full'}
 workdir="${COMOUT}/graphics/${WGF}/${tile}"
-zipdir="${COMOUT}/nclprd"
+zipdir="${COMOUT}/nclprd/${WGF}"
 rm -rf "${workdir}"
 mkdir -p "${workdir}"
 cd "${pygrafdir}" || exit 1
