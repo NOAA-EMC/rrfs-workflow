c6192dc90fe04af7cedea3c23837755e37efff91
benjamin blake <benjamin.blake@dlogin07.dogwood.wcoss2.ncep.noaa.gov>
2026-07-10 14:03:04 +0000

    Add exception handling to prdgen job to prevent wgrib2 errors

--- ush/prdgen/rrfs_prdgen_subpiece.sh
+++ ush/prdgen/rrfs_prdgen_subpiece.sh
@@ -46,7 +46,7 @@ fi
 
 # Use different parm file for each subpiece
 if [[ "$inputfile" =~ "prslev" ]]; then
-  wgrib2 $comout/${inputfile} | grep -F -f ${parmfile} | wgrib2 -i -grib inputs.grib${domain} $comout/${inputfile}
+  wgrib2 $comout/${inputfile} | grep -F -f ${parmfile} | wgrib2 -i -grib inputs.grib${domain} $comout/${inputfile} >>$pgmout 2>>errfile
   wgrib2 inputs.grib${domain} -new_grid_vectors "UGRD:VGRD:USTM:VSTM" -submsg_uv inputs.grib${domain}.uv
 else
   wgrib2 $comout/${inputfile} -new_grid_vectors "UGRD:VGRD:USTM:VSTM" -submsg_uv inputs.grib${domain}.uv
@@ -56,7 +56,7 @@ wgrib2 inputs.grib${domain}.uv -set_bitmap 1 -set_grib_type ${compress_type} \
   -new_grid_winds grid -new_grid_vectors "UGRD:VGRD:USTM:VSTM" \
   -new_grid_interpolation neighbor \
   -if ":(WEASD|APCP|NCPCP|ACPCP|SNOD):" -new_grid_interpolation budget -fi \
-  -new_grid ${gridspecs} ${domain}_${subpiece}.grib2
+  -new_grid ${gridspecs} ${domain}_${subpiece}.grib2 >>$pgmout 2>>errfile
 
 #export err=$?; err_chk
 
