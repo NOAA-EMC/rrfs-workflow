e17def2a3eb43c8804b61c2ce80d4d4a1a6dd23a
Matthew Pyle <matthew.pyle@noaa.gov>
2026-07-10 14:56:17 +0000

    Wraps exrrfs_prep_cyc.sh mail.py sends with a SENDMAIL variable.

--- scripts/exrrfs_prep_cyc.sh
+++ scripts/exrrfs_prep_cyc.sh
@@ -375,7 +375,9 @@ else
       done
       if [ ${fallback_enable} == "YES" ]; then
         print_info_msg "$VERBOSE" "WARNING: cannot find restart files in previous 1 hour, proceeding fallback for older cycle"
-        echo "WARNING: cannot find restart files in previous 1 hour, proceeding fallback for older cycle" | mail.py -s "RRFS prep_cyc fallback" -c ${MAILTO}
+	if [ ${SENDMAIL} == "YES" ]; then
+          echo "WARNING: cannot find restart files in previous 1 hour, proceeding fallback for older cycle" | mail.py -s "RRFS prep_cyc fallback" -c ${MAILTO}
+	fi
         fg_restart_dirname=forecast
         restart_prefix="${YYYYMMDD}.${HH}0000."
         if [ ${BKTYPE} -eq 2 ] && [ "${DO_ENSEMBLE}" = "FALSE" ]; then  #det cycle 09/21z start from n=1
@@ -400,7 +402,9 @@ else
              break
            else
              print_info_msg "$VERBOSE" "WARNING: fallback cannot find restart files in previous ${n} hour"
-             echo "WARNING: fallback cannot find restart files in previous ${n} hour" | mail.py -s "RRFS prep_cyc fallback" -c ${MAILTO}
+	     if [ ${SENDMAIL} == "YES" ]; then
+               echo "WARNING: fallback cannot find restart files in previous ${n} hour" | mail.py -s "RRFS prep_cyc fallback" -c ${MAILTO}
+	     fi
     	   fi
            n=$((n + ${DA_CYCLE_INTERV}))
         done
