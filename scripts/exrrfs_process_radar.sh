3c950f77c8754e5008d36ad5e63f435cee72f2b4
James Polly <james.polly@noaa.gov>
2026-07-20 18:13:45 +0000

    add email for missing radar data

--- scripts/exrrfs_process_radar.sh
+++ scripts/exrrfs_process_radar.sh
@@ -249,6 +249,17 @@ for bigmin in ${RADARREFL_TIMELEVEL[@]}; do
      print_info_msg "$VERBOSE" "NSSL grib2 file levels = $numgrib2"
   else
      echo "WARNING: Not enough radar reflectivity files in ${NSSL} available for loop ${bigmin}.  Will continue without it (radar data not critical)"
+     cat << EOF > radar_warning.txt
+WARNING: Not enough radar reflectivity files are available at the below path:
+${NSSL}
+
+RRFS cycle will continue without these files.
+
+No immediate impact to integrity of delivered products.
+
+Ops action: identify cause of missing radar data at the above path.
+EOF
+     mail.py -s "Missing Radar Data of Opportunity for RRFS" -v ${MAIL_TO} < radar_warning.txt
      continue
   fi
   #
