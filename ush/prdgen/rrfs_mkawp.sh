4f4f6664032a80c61f54ea0daba2ff861626e48c
MatthewPyle-NOAA <48285220+MatthewPyle-NOAA@users.noreply.github.com>
2026-08-19 11:22:34 -0400

    [release/v1.0] Interpolates AWIPS products away from rotated lat-lon projection (#1650)
    * Beginning of alternate idea to put interpolation into rrfs_mkawp to
    limit the amount that is done, and not have it available on COM (just
    going to AWIPS).
    * Possible change to interpolate to NPS projection as AWIPS products
    are extracted.
    * Fixes based on testing new script logic
    * Update after first test failed.
    * Slightly tweaked output domain to shrink filesize a bit.
    * Makes a comment line a statement rather than question.
    * Changes griddef to gridspecs in response to review comment.

--- ush/prdgen/rrfs_mkawp.sh
+++ ush/prdgen/rrfs_mkawp.sh
@@ -21,14 +21,32 @@ fhr=$1
 inputfile=$2
 gridspacing=$3
 
+compress_type=c3
+
 runRRFS="000 003 006 009 012 015 018 021 024 027 030 033 036 039 042 045 048 051 054 057 060 066 072 078 084"
 if  echo $runRRFS |grep $fhr;
 then
   # Processing AWIPS grid (RRFS North America grid - 3 km or 13 km)
   export INPUTfile=${COMOUT}/rrfs.t${cyc}z.${inputfile}.${gridspacing}.f${fhr}.na.grib2
 
+  # Add an interpolation step here
+  if [ $gridspacing == "3km" ]; then
+    gridspecs="nps:248:60 -151.5:4680:3000 6.5:2830:3000"
+  elif [ $gridspacing == "13km" ] ; then
+    gridspecs="nps:248:60 -151.5:1080:13000 6.5:653:13000"
+  else
+    err_exit "FATAL ERROR: seeing an improper gridspacing definition in rrfs_mkawp.sh: $gridspacing"
+  fi
+
   # Only grab records that need WMO headers for AWIPS
-  $WGRIB2 ${INPUTfile} | grep -F -f ${PARMrrfs}/wmo/rrfsparams_${gridspacing} | $WGRIB2 -i ${INPUTfile} -new_grid_winds grid -set_grib_type same -grib rrfs.t${cyc}z.${inputfile}.${gridspacing}.f${fhr}.na.grib2
+ 
+  $WGRIB2 ${INPUTfile} | grep -F -f ${PARMrrfs}/wmo/rrfsparams_${gridspacing} | $WGRIB2 -i ${INPUTfile} -new_grid_winds grid -set_grib_type same -grib input.t${cyc}z.${inputfile}.${gridspacing}.f${fhr}.na.grib2
+
+  $WGRIB2 input.t${cyc}z.${inputfile}.${gridspacing}.f${fhr}.na.grib2  -set_bitmap 1 -set_grib_type ${compress_type} \
+  -new_grid_winds grid -new_grid_vectors "UGRD:VGRD:USTM:VSTM" \
+  -new_grid_interpolation neighbor \
+  -if ":(WEASD|APCP|NCPCP|ACPCP|SNOD):" -new_grid_interpolation budget -fi \
+  -new_grid ${gridspecs} rrfs.t${cyc}z.${inputfile}.${gridspacing}.f${fhr}.na.grib2
 
   # Run tocgrib2
 
