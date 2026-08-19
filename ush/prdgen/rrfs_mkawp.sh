edf0808d0220460bc08e8b5b5f53fe7ebb10d31c
Matthew Pyle <matthew.pyle@noaa.gov>
2026-08-19 14:47:06 +0000

    Changes griddef to gridspecs in response to review comment.

--- ush/prdgen/rrfs_mkawp.sh
+++ ush/prdgen/rrfs_mkawp.sh
@@ -31,9 +31,9 @@ then
 
   # Add an interpolation step here
   if [ $gridspacing == "3km" ]; then
-    griddef="nps:248:60 -151.5:4680:3000 6.5:2830:3000"
+    gridspecs="nps:248:60 -151.5:4680:3000 6.5:2830:3000"
   elif [ $gridspacing == "13km" ] ; then
-    griddef="nps:248:60 -151.5:1080:13000 6.5:653:13000"
+    gridspecs="nps:248:60 -151.5:1080:13000 6.5:653:13000"
   else
     err_exit "FATAL ERROR: seeing an improper gridspacing definition in rrfs_mkawp.sh: $gridspacing"
   fi
@@ -46,7 +46,7 @@ then
   -new_grid_winds grid -new_grid_vectors "UGRD:VGRD:USTM:VSTM" \
   -new_grid_interpolation neighbor \
   -if ":(WEASD|APCP|NCPCP|ACPCP|SNOD):" -new_grid_interpolation budget -fi \
-  -new_grid ${griddef} rrfs.t${cyc}z.${inputfile}.${gridspacing}.f${fhr}.na.grib2
+  -new_grid ${gridspecs} rrfs.t${cyc}z.${inputfile}.${gridspacing}.f${fhr}.na.grib2
 
   # Run tocgrib2
 
