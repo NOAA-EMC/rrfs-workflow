05ad3c1fa5edf33664187bb6ddfe030d7cd611b1
guoqing.ge <Guoqing.Ge@noaa.gov>
2026-08-23 13:47:21 -0600

    update modulefiles/rrfs so that we can build chemrrfs

--- modulefiles/rrfs/ursa.intel.lua
+++ modulefiles/rrfs/ursa.intel.lua
@@ -7,7 +7,7 @@ prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.9.1/envs/ue-oneap
 
 load("stack-oneapi/2024.2.1")
 load("stack-intel-oneapi-mpi/2021.13")
-
+load("intel-oneapi-mkl/2024.2.1")
 load("cmake/3.27.9")
 load("parallel-netcdf/1.12.3")
 load("parallelio/2.6.2")
@@ -17,16 +17,14 @@ load("libpng/1.6.37")
 setenv("CMAKE_C_COMPILER", "mpiicc")
 setenv("CMAKE_CXX_COMPILER", "mpiicpc")
 setenv("CMAKE_Fortran_COMPILER", "mpiifort")
---setenv("SERIAL_CC", "icx")
--- note for future upgrade
---setenv("CMAKE_C_COMPILER", "mpiicx")
---setenv("CMAKE_CXX_COMPILER", "mpiicpx")
---setenv("CMAKE_Fortran_COMPILER", "mpiifx")
 
 if mode() == "load" then
-  --setenv("PNETCDF", os.getenv("PARALLEL_NETCDF_ROOT"))
   setenv("PNETCDF", os.getenv("parallel_netcdf_ROOT"))
+  setenv("NetCDF_C_ROOT", os.getenv("netcdf_c_ROOT"))
+  setenv("NetCDF_FORTRAN_ROOT", os.getenv("netcdf_fortran_ROOT"))
 end
 if mode() == "unload" then
   unsetenv("PNETCDF")
+  unsetenv("NetCDF_C_ROOT")
+  unsetenv("NetCDF_FORTRAN_ROOT")
 end
