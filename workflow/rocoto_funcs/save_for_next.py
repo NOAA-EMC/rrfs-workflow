3c37e56bebfc03b8ff5c6f9e73136d0ccf990913
Guoqing Ge <guoqing.ge@noaa.gov>
2026-08-26 11:40:51 -0600

    [v2.1.4-5.7] Make the `save_for_next` task smarter enough to accommodate different forecast lengths at different cycles (#1676)
    * add smart_save4next_groups.py
    * update save_for_next.py accordingly
    * address the linter comments
    * address the linter comment
    * ignore XML differences as they are expected

--- workflow/rocoto_funcs/save_for_next.py
+++ workflow/rocoto_funcs/save_for_next.py
@@ -5,18 +5,18 @@ from rocoto_funcs.base import xml_task, get_cascade_env
 # begin of fcst --------------------------------------------------------
 
 
-def save_for_next(xmlFile, expdir, do_ensemble=False, do_spinup=False):
-    cyc_interval = os.getenv('CYC_INTERVAL')
-    cycledefs = 'prod'
+def save_for_next(xmlFile, expdir, dcGrpInfo, do_ensemble=False):
     meta_id = 'save_for_next'
+    fhr = dcGrpInfo["fhr"]
+    task_id = f'{meta_id}_f{fhr:02d}'
+    cycledefs = dcGrpInfo['cycledef']
+    cyc_interval = os.getenv('CYC_INTERVAL')
     mpasout_interval = os.getenv('MPASOUT_INTERVAL', '1') or cyc_interval
-    mpasout_timelevels = os.getenv('MPASOUT_TIMELEVELS', '') or mpasout_interval
-    group_indices = ' '.join(f'{int(i):02d}' for i in mpasout_timelevels.split() if int(i) != 0)
 
     # Task-specific EnVars beyond the task_common_vars
     dcTaskEnv = {
-        'MPASOUT_INTERVAL': os.getenv('MPASOUT_INTERVAL', '1'),
-        'FCST_HR': f'#mpasout_fhr#',
+        'MPASOUT_INTERVAL': mpasout_interval,
+        'FCST_HR': f'{fhr}',
     }
 
     if do_ensemble:
@@ -25,22 +25,14 @@ def save_for_next(xmlFile, expdir, do_ensemble=False, do_spinup=False):
 
     dcTaskEnv['KEEPDATA'] = get_cascade_env(f"KEEPDATA_{meta_id}".upper()).upper()
 
-    # metatask (nested or not)
-    meta_bgn = f'''
-<metatask name="{meta_id}">
-<var name="mpasout_fhr">{group_indices}</var>'''
-    meta_end = f'\
-</metatask>\n'
-    task_id = f'{meta_id}_f#mpasout_fhr#'
-
     # dependencies
     if do_ensemble:
         datadep = ""
         for i in range(1, int(ens_size) + 1):
             memdirstr = f'/mem{i:03d}'
-            datadep = datadep + f'''\n    <datadep age="00:00:10"><cyclestr>&DATAROOT;/@Y@m@d/&RUN;_fcst_@H_&rrfs_ver;/&WGF;{memdirstr}</cyclestr><cyclestr offset="#mpasout_fhr#:00:00">/mpasout.@Y-@m-@d_@H.@M.@S.nc.done</cyclestr></datadep>'''
+            datadep = datadep + f'''\n    <datadep age="00:00:10"><cyclestr>&DATAROOT;/@Y@m@d/&RUN;_fcst_@H_&rrfs_ver;/&WGF;{memdirstr}</cyclestr><cyclestr offset="{fhr}:00:00">/mpasout.@Y-@m-@d_@H.@M.@S.nc.done</cyclestr></datadep>'''
     else:
-        datadep = f'''\n    <datadep age="00:00:10"><cyclestr>&DATAROOT;/@Y@m@d/&RUN;_fcst_@H_&rrfs_ver;/&WGF;</cyclestr><cyclestr offset="#mpasout_fhr#:00:00">/mpasout.@Y-@m-@d_@H.@M.@S.nc.done</cyclestr></datadep>'''
+        datadep = f'''\n    <datadep age="00:00:10"><cyclestr>&DATAROOT;/@Y@m@d/&RUN;_fcst_@H_&rrfs_ver;/&WGF;</cyclestr><cyclestr offset="{fhr}:00:00">/mpasout.@Y-@m-@d_@H.@M.@S.nc.done</cyclestr></datadep>'''
 
     timedep = ""
     realtime = os.getenv("REALTIME", "false")
@@ -55,5 +47,5 @@ def save_for_next(xmlFile, expdir, do_ensemble=False, do_spinup=False):
   </dependency>'''
 
     #
-    xml_task(xmlFile, expdir, task_id, cycledefs, dcTaskEnv, dependencies, True, meta_id, meta_bgn, meta_end, command_id="SAVE_FOR_NEXT")
+    xml_task(xmlFile, expdir, task_id, cycledefs, dcTaskEnv, dependencies, command_id="SAVE_FOR_NEXT")
 # end of fcst --------------------------------------------------------
