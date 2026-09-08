a59d8875701e1b5feac4f6eb41a1e7d35c868a87
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2026-09-08 14:54:14 -0400

    Add more descriptive warning message

--- workflow/setup_rocoto.py
+++ workflow/setup_rocoto.py
@@ -49,7 +49,8 @@ mesh = os.getenv("MESH_NAME", "conus3km")
 # Check compatibility of setup with nonvar cloud analysis
 if os.getenv("DO_NONVAR_CLOUD_ANA", "FALSE").upper() == "TRUE":
     if mesh not in ["conus3km", "conus3.5km", "south3.5km", "conus12km"]:
-        print(f'WARN: {mesh} is not compatible with the nonvar cloud analysis')
+        print(f'WARN: {mesh} has not been fully tested with the nonvar cloud analysis')
+        print(f'WARN: Configuration options in config_resources/config.meshdep may also be missing for {mesh}')
 
 # create comroot (no matter exists or not)
 comroot = get_required_env('COMROOT')
