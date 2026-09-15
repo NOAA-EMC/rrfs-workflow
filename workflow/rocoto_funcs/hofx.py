96c0081c424cc3efbfb76b2868d3c33297ef4306
guoqing.ge <Guoqing.Ge@noaa.gov>
2026-09-15 19:14:41 -0400

    update taskdep for mpassit.py and hofx.py

--- workflow/rocoto_funcs/hofx.py
+++ workflow/rocoto_funcs/hofx.py
@@ -32,7 +32,12 @@ def hofx(xmlFile, expdir, do_spinup=False):
     #
     dependencies = f'''
   <dependency>{timedep}
-    <taskdep task="fcst"/>
+    <or>
+      <taskdep task="fcst"/>
+      <taskdep task="fcst_h"/>
+      <taskdep task="fcst_hh"/>
+      <taskdep task="fcst_hhh"/>
+    </or>
   </dependency>'''
     #
     xml_task(xmlFile, expdir, task_id, cycledefs, dcTaskEnv, dependencies, command_id="hofx")
